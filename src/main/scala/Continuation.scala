33b1f795a57182703816bc237e336d46ac661d12
raulraja <raulraja@gmail.com>
2021-11-18 18:03:52 +0100

    Remove Coroutine wrapping class

--- src/main/scala/Continuation.scala
+++ src/main/scala/Continuation.scala
@@ -25,28 +25,3 @@ inline def fold[R, A, B](inline fc: A |> Control[R])(inline f: R => B, inline g:
   }
   result.asInstanceOf[B]
 }
-
-private[fx] class Coroutine[R, A](fc: Control[R] ?=> A) /* extends Cont[R, A] */ {
-  def fold[B](f: R => B, g: A => B): B = {
-    var result: A | B | Null = null
-    new ContinuationScope("scala-fx continuation scope") with Control[R] {
-
-      val cont = new java.lang.Continuation(
-        this,
-        () => {
-          result = g(fc(using this))
-        }
-      )
-
-      extension (r: R)
-        def shift[A]: A = {
-          result = f(r)
-          Continuation.`yield`(this)
-          result.asInstanceOf[A]
-        }
-
-      cont.run
-    }
-    result.asInstanceOf[B]
-  }
-}
