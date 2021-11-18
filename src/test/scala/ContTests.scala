2770f6a1b7cc5a37b920dce75930d3031a5f21a0
raulraja <raulraja@gmail.com>
2021-11-18 18:01:40 +0100

    redo tests

--- src/test/scala/ContTests.scala
+++ src/test/scala/ContTests.scala
@@ -3,30 +3,24 @@ package fx
 import org.scalacheck.Properties
 import org.scalacheck.Prop.forAll
 
-object ContTests extends Properties("Cont laws") :
+object ContTests extends Properties("Cont laws"):
 
-  // property("Can short-circuit immediately from nested blocks") =
-  //   forAll { (s: String) =>
-  //     cont[String, Int] {
-  //       cont[Int, Int] {
-  //         shift(s)
-  //       }.fold(identity, identity)
-  //     }.fold(identity, identity) == s
-  //   }
+  property("Can short-circuit immediately from nested blocks") = forAll {
+    (s: String) =>
+      def outer: Int |> Control[String] = 1
+      def inner: Int |> Control[String] =
+        s.shift[Int] + outer + 1
+      run(inner) == s
+  }
 
-  // property("happy path") =
-  //   forAll { (n: Int) =>
-  //     cont[String, Int] {
-  //       n
-  //     }.fold(identity, identity) == n
-  //   }
+  property("happy path") = forAll { (n: Int) =>
+    def effect: Int |> Control[String] = n
+    run(effect) == n
+  }
 
-  // property("short-circuit") =
-  //   forAll { (s: String) =>
-  //     cont[String, Int] {
-  //       shift(s)
-  //     }.fold(identity, identity) == s
-  //   }
-  
+  property("short-circuit") = forAll { (s: String) =>
+    def effect: Int |> Control[String] = s.shift
+    run(effect) == s
+  }
 
 end ContTests
