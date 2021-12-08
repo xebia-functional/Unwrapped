bbfbac8f9298d1bde022c93b588dfadbdf8dea66
raulraja <raulraja@gmail.com>
2021-12-08 11:42:08 +0100

    Console syntax

--- src/main/scala/Console.scala
+++ src/main/scala/Console.scala
@@ -8,66 +8,85 @@ import scala.io.StdIn.readLine
   "Missing capability:\n* Console"
 )
 trait Console:
-  def consoleWrite(s: String): Unit
-  def consoleReadLine(): Option[String]
 
-extension (c: Console)
-  def consoleWriteLine(s: String): Unit = 
-    c.consoleWrite(s + "\n")
+  def read(): String | Null
+
+  extension (s: String)
+    def write(): Unit
+    def writeLine(): Unit
+
+object Console:
+  given default: Console = StandardConsole
+
+/**
+ * If your method is not an extension then it needs side syntax
+ * like these or users would need to summon.
+ **/
+def read(): (String | Null) * Console =
+  summon[Console].read()
 
 object StandardConsole extends Console:
-  def consoleWrite(s: String): Unit = print(s)
-  def consoleReadLine(): Option[String] = Some(readLine())
+
+  def read(): String | Null = readLine()
+
+  extension (s: String)
+    def write(): Unit = print(s)
+    def writeLine(): Unit = println(s)
 
 class FakeConsole(var input: String) extends Console:
   var output: String = ""
 
-  def consoleWrite(s: String): Unit = output += s
-  def consoleReadLine(): Option[String] =
-    if input.isEmpty then
-      None
+  def read(): String | Null =
+    if input.isEmpty then null
     else
       input.split('\n') match
         case Array(r, rest*) =>
           input = rest.mkString("\n")
-          Some(r)
+          r
         case _ =>
-          None
+          null
 
-// here I need to use summon[Console] over and over...
+  extension (s: String)
+    def write(): Unit = output += s
+    def writeLine(): Unit = output += (s + "\n")
+
 def program1: String * Console * Errors[String] =
-  summon[Console].consoleWrite("what is your name?")
-  summon[Console].consoleReadLine() match
-    case None => 
+  "what is your name?".write()
+  read() match
+    case null =>
       "end of input".raise
-    case Some("") =>
-      summon[Console].consoleWriteLine("empty name")
+    case "" =>
+      "empty name".writeLine()
       program1
-    case Some("me") =>
-      summon[Console].consoleWriteLine("wrong name")
+    case "me" =>
+      "wrong name".writeLine()
       "wrong name".raise
-    case Some(name) =>
+    case name =>
       name
 
-// create some boilerplate-less methods
-def consoleWrite(s: String): Unit * Console =
-  summon[Console].consoleWrite(s)
-def consoleWriteLine(s: String): Unit * Console =
-  summon[Console].consoleWriteLine(s)
-def consoleReadLine(): Option[String] * Console =
-  summon[Console].consoleReadLine()
-
-// here I need to use summon[Console] over and over...
 def program2: String * Console * Errors[String] =
-  consoleWrite("what is your name?")
-  consoleReadLine() match
-    case None => 
+  "what is your name?".write()
+  read() match
+    case null =>
       "end of input".raise
-    case Some("") =>
-      consoleWriteLine("empty name")
+    case "" =>
+      "empty name".writeLine()
       program2
-    case Some("me") =>
-      consoleWriteLine("wrong name")
+    case "me" =>
+      "wrong name".writeLine()
       "wrong name".raise
-    case Some(name) =>
+    case name =>
       name
+
+@main def consoleStandard() =
+  import fx.runtime
+
+  val value: String =
+    run(program2)
+
+@main def consoleFake() =
+  import fx.runtime
+  given Console = FakeConsole("Fake Input!")
+  val value: String =
+    run(program2)
+  println(value)
