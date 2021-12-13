f03c14eb139a8735a24160a5282ef904ceeab15a
Qohat <qohatpp@gmail.com>
2021-12-13 18:38:53 -0500

    Runtime tests, removed unnecessary run call

--- src/test/scala/RuntimeTests.scala
+++ src/test/scala/RuntimeTests.scala
@@ -7,19 +7,19 @@ import fx.*
 object RuntimeTests extends Properties("Runtime Tests"):
   property("Either binding toOption") = forAll { (a: Int, b: Int) =>
     val effect: Int * Control[Nothing] = Right(a).bind + Right(b).bind
-    run(effect).toOption == Some(a + b)
+    toOption(effect) == Some(a + b)
   }
 
   property("Option binding toEither") = forAll { (a: Int, b: Int) =>
     val effect: Int * Control[None.type] =
       Some(a).bind + Some(b).bind
-    run(effect).toEither == Right(a + b)
+    toEither(effect) == Right(a + b)
   }
 
   property("Binding two values of different types toEither") = forAll {
     (a: Int, b: Int) =>
       val effect: Int * Control[None.type] = Right(a).bind + Some(b).bind
-      run(effect).toEither == Right(a + b)
+      toEither(effect) == Right(a + b)
   }
 
   property("Short-circuiting with Either.Left toEither") = forAll {
