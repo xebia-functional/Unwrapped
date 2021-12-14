c8ea018e1a122e656422de4383fdabd90a1da8c1
Qohat Pretel Polo <qohatpp@gmail.com>
2021-12-14 11:23:01 -0500

    toOption - toEither runtime tests (#10)
    * Short-circuiting tests scenarios
    
    * Runtime tests, removed unnecessary run call
    
    * Removed unnecesary imports and a way for removing cats dependency

--- src/test/scala/RuntimeTests.scala
+++ src/test/scala/RuntimeTests.scala
@@ -1,24 +1,37 @@
 package fx
 
-import cats.syntax.option._
 import org.scalacheck.Properties
 import org.scalacheck.Prop.forAll
 
 object RuntimeTests extends Properties("Runtime Tests"):
   property("Either binding toOption") = forAll { (a: Int, b: Int) =>
     val effect: Int * Control[Nothing] = Right(a).bind + Right(b).bind
-    run(effect).toOption == Some(a + b)
+    toOption(effect) == Some(a + b)
   }
 
   property("Option binding toEither") = forAll { (a: Int, b: Int) =>
     val effect: Int * Control[None.type] =
       Some(a).bind + Some(b).bind
-    run(effect).toEither == Right(a + b)
+    toEither(effect) == Right(a + b)
   }
 
   property("Binding two values of different types toEither") = forAll {
     (a: Int, b: Int) =>
       val effect: Int * Control[None.type] = Right(a).bind + Some(b).bind
-      run(effect).toEither == Right(a + b)
+      toEither(effect) == Right(a + b)
   }
+
+  property("Short-circuiting with Either.Left toEither") = forAll {
+    (n: Int, s: String) =>
+      val effect: Int * Control[String | None.type] =
+        Left[String, Int](s).bind + Some(n).bind
+      toEither(effect) == Left[String, Int](s)
+  }
+
+  property("Short-circuiting with Option.None toOption") = forAll { (n: Int) =>
+    val effect: Int * Control[None.type] =
+      Right(n).bind + Option.empty[Int].bind
+    toOption(effect) == None
+  }
+
 end RuntimeTests
