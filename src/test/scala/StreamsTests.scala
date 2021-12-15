c80f139889a139663a47582813c8a6b829d55bd2
raulraja <raulraja@gmail.com>
2021-12-15 14:20:01 +0100

    scalafmt take2

--- src/test/scala/StreamsTests.scala
+++ src/test/scala/StreamsTests.scala
@@ -5,9 +5,7 @@ import org.scalacheck.Prop.forAll
 
 object StreamsTests extends Properties("Streams tests"):
 
-  property("toList") = forAll { (list: List[Int]) =>
-    streamOf(list*).toList == list
-  }
+  property("toList") = forAll { (list: List[Int]) => streamOf(list*).toList == list }
 
   property("indexed") = forAll { (list: List[Int]) =>
     streamOf(list*).zipWithIndex.toList == list.zipWithIndex
