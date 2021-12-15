f05d047b9f16f848374fbfdc40293193ed4f0b78
raulraja <raulraja@gmail.com>
2021-12-15 14:35:46 +0100

    add tests for map and flatMap

--- src/test/scala/StreamsTests.scala
+++ src/test/scala/StreamsTests.scala
@@ -5,10 +5,18 @@ import org.scalacheck.Prop.forAll
 
 object StreamsTests extends Properties("Streams tests"):
 
+  property("send/receive") = forAll { (n: Int) => streamed { send(n) }.toList == List(n) }
+
   property("toList") = forAll { (list: List[Int]) => streamOf(list*).toList == list }
 
   property("indexed") = forAll { (list: List[Int]) =>
     streamOf(list*).zipWithIndex.toList == list.zipWithIndex
+  }
+
+  property("map") = forAll { (n: Int) => streamOf(n).map(_ + 1).toList == List(n + 1) }
+
+  property("flatMap") = forAll { (n: Int) =>
+    streamOf(n).flatMap(n => streamOf(n + 1)).toList == List(n + 1)
   }
 
 end StreamsTests
