d4dc81cc232d782d60024f782c08f397e646bd46
franciscodr <francisco.d@47deg.com>
2021-12-16 12:04:09 +0100

    Add tests for flatten and fold

--- scala-fx/src/test/scala/StreamsTests.scala
+++ scala-fx/src/test/scala/StreamsTests.scala
@@ -5,12 +5,25 @@ import org.scalacheck.Prop.forAll
 
 object StreamsTests extends Properties("Streams tests"):
 
-  property("send/receive") = forAll { (n: Int) => streamed { send(n) }.toList == List(n) }
+  property("send/receive") = forAll { (n: Int) => streamOf(n).toList == List(n) }
 
   property("toList") = forAll { (list: List[Int]) => streamOf(list*).toList == list }
 
   property("zipWithIndex") = forAll { (list: List[Int]) =>
     streamOf(list*).zipWithIndex.toList == list.zipWithIndex
+  }
+
+  property("flatten: identity") = forAll { (n: Int) =>
+    streamOf(n).map(i => streamOf(i)).flatten.toList == List(n)
+  }
+
+  property("flatten") = forAll { (list: List[Int]) =>
+    streamOf(list*).map(i => streamOf(i, i + 1)).flatten.toList == list.flatMap(i =>
+      List(i, i + 1))
+  }
+
+  property("fold") = forAll { (initial: Int, a: Int, b: Int) =>
+    streamOf(a, b).fold(initial, _ + _).toList == List(initial, initial + a, initial + a + b)
   }
 
   property("map") = forAll { (n: Int) => streamOf(n).map(_ + 1).toList == List(n + 1) }
