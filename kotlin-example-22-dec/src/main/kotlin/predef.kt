6d09c362f3307eedefd10cf3d21d6629d4b2733a
Simon Vergauwen <nomisRev@users.noreply.github.com>
2021-12-27 19:40:36 +0100

    Update Clock

--- kotlin-example-22-dec/src/main/kotlin/predef.kt
+++ kotlin-example-22-dec/src/main/kotlin/predef.kt
@@ -1,7 +1,11 @@
-import java.time.Instant
+@file:OptIn(ExperimentalTime::class)
+
+import arrow.fx.coroutines.timeInMillis
+import kotlin.time.Duration
+import kotlin.time.ExperimentalTime
+import kotlin.time.TimedValue
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.FlowCollector
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
 
 suspend fun <A> FlowCollector<A>.emitAll(s: Sequence<A>): Unit =
@@ -35,10 +39,31 @@ typealias Processor<A, B> = (Flow<A>) -> Flow<B>
 /** An inline operator that applies a [Processor] to a [Flow] in a fluent API.  */
 inline fun <A, B> Flow<A>.through(processor: Processor<A, B>): Flow<B> = processor(this)
 
-interface Clock {
-  suspend fun now(): Instant
+interface Clock : Real, Monotonic {
+  companion object {
+    val Default: Clock = object : Clock,
+      Real by Real.Default,
+      Monotonic by Monotonic.Default {}
+  }
+}
 
-  companion object Default : Clock {
-    override suspend fun now(): Instant = Instant.now()
+fun interface Monotonic {
+  suspend fun epochMillis(): Long
+
+  companion object {
+    val Default = Monotonic { timeInMillis() }
+  }
+}
+
+fun interface Real {
+  suspend fun nanoTime(): Long
+
+  suspend fun <A> timed(f: suspend () -> A): TimedValue<A> {
+    val start = nanoTime()
+    return TimedValue(f(), Duration.nanoseconds(nanoTime() - start))
+  }
+
+  companion object {
+    val Default = Real { System.nanoTime() }
   }
 }
