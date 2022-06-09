baf3735e2634c5d30c2854f0c6c74f5074e098f2
franciscodr <francisco.d@47deg.com>
2022-06-09 16:10:15 +0200

    Add bind implementation for Either.Right

--- scala-fx/src/main/scala/fx/Bind.scala
+++ scala-fx/src/main/scala/fx/Bind.scala
@@ -2,9 +2,11 @@ package fx
 
 extension [R, A](fa: Either[R, A]) def bind(using Errors[R]): A = fa.fold(_.shift, identity)
 
+extension [R, A](fa: Right[R, A]) def bind: A = fa.value
+
 extension [R, A](fa: List[Either[R, A]]) def bind(using Errors[R]): List[A] = fa.map(_.bind)
 
 extension [A](fa: Option[A])
   def bind(using Errors[None.type]): A = fa.fold(None.shift)(identity)
 
-extension [A](fa: Some[A]) def bind: A = fa.get
+extension [A](fa: Some[A]) def bind: A = fa.value
