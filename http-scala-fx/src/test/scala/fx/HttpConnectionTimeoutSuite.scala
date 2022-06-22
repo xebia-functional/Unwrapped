376a181524aa9d5f12f4db204fd7e5ab2839bf66
Jack Viers <jackcviers@gmail.com>
2022-06-22 14:39:44 -0500

    Fixes value usage.

--- http-scala-fx/src/test/scala/fx/HttpConnectionTimeoutSuite.scala
+++ http-scala-fx/src/test/scala/fx/HttpConnectionTimeoutSuite.scala
@@ -1,29 +1,26 @@
 package fx
 
-import scala.compiletime.testing.typeChecks
-import scala.concurrent.duration.FiniteDuration
-import scala.concurrent.duration.MINUTES
-
 import munit.ScalaCheckSuite
 import org.scalacheck.Prop._
 
-class HttpConnectionTimeoutSuite extends ScalaCheckSuite {
+import scala.annotation.nowarn
+
+class HttpConnectionTimeoutSuite extends ScalaCheckSuite:
 
   property("Any positive long can be a HttpConnectionTimeout") {
     forAll { (i: Long) =>
       (i > 0) ==> {
-        run(HttpConnectionTimeout.of(i)) match {
+        @nowarn // value will not resolve without the fruitless typecheck
+        val result = run(HttpConnectionTimeout.of(i)) match
           case s: String => fail(s"unexpected shift to error: $s")
-          case _ => passed
-        }
+          case x: HttpConnectionTimeout => x.value == i
+        result
       }
       (i <= 0) ==> {
-        run(HttpConnectionTimeout.of(i)) match {
+        run(HttpConnectionTimeout.of(i)) match
           case s: String => s == "Durations must be positive"
           case _ =>
             fail("0 or negative numbers should have failed")
-        }
       }
     }
   }
-}
