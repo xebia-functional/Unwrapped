c3f0224814d909c3b4b0ae46d6eaa5b7c1a79368
Jack Viers <jackcviers@gmail.com>
2022-06-22 11:27:32 -0500

    Abandoning staged for now, as it doesn't seem to work in the tests.

--- http-scala-fx/src/main/scala/fx/HttpConnectionTimeout.scala
+++ http-scala-fx/src/main/scala/fx/HttpConnectionTimeout.scala
@@ -1,25 +1,28 @@
 package fx
 
-import scala.concurrent.duration.FiniteDuration
-import scala.concurrent.duration.Duration
-import scala.concurrent.duration.SECONDS
-
 /**
  * Models http connection timeouts as finite durations
  */
-opaque type HttpConnectionTimeout = FiniteDuration
+opaque type HttpConnectionTimeout = Long
+
+extension (h: HttpConnectionTimeout)
+  def toLong:Long = h
 
 object HttpConnectionTimeout:
   /**
    * @constructor
    */
   inline def apply(durationInSeconds: Long): HttpConnectionTimeout =
-    requires(durationInSeconds > 0, "Durations must be positive")
-    FiniteDuration(durationInSeconds, SECONDS)
+    requires(durationInSeconds > 0, "Durations must be positive", durationInSeconds)
 
-
-
+  def of(durationInSeconds: Long): Either[String, HttpConnectionTimeout] =
+    if (durationInSeconds > 0)
+      Right(durationInSeconds)
+    else
+      Left("Durations must be positive")
+  
   /**
-    * Default connection timeout is 30 seconds
-    */
-  given defaultHttpConnectionTimeout: HttpConnectionTimeout = HttpConnectionTimeout(30)
+   * Default connection timeout is 30 seconds
+   */
+  given defaultHttpConnectionTimeout: HttpConnectionTimeout =
+    HttpConnectionTimeout(30)
--- http-scala-fx/src/test/scala/fx/HttpConnectionTimeoutSuite.scala
+++ http-scala-fx/src/test/scala/fx/HttpConnectionTimeoutSuite.scala
@@ -4,8 +4,17 @@ import scala.compiletime.testing.typeChecks
 import scala.concurrent.duration.FiniteDuration
 import scala.concurrent.duration.MINUTES
 
-import munit.fx.ScalaFXSuite
+import munit.ScalaCheckSuite
+import org.scalacheck.Prop._
 
-class HttpConnectionTimeoutSuite extends ScalaFXSuite {
+class HttpConnectionTimeoutSuite extends ScalaCheckSuite {
 
+  property("Any positive long can be a HttpConnectionTimeout") {
+    forAll { (i: Long) =>
+      (i > 0) ==> {
+        HttpConnectionTimeout.of(i).right.get.toLong == i
+      }
+      (i <= 0) ==> { HttpConnectionTimeout.of(i) == Left("Durations must be positive") }
+    }
+  }
 }
