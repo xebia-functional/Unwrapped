18ba08f17d8bf445eb1f87e35ae8b9aa0cafab3f
Jack Viers <jackcviers@gmail.com>
2022-06-22 11:35:47 -0500

    fx application of of for runtime

--- http-scala-fx/src/test/scala/fx/HttpConnectionTimeoutSuite.scala
+++ http-scala-fx/src/test/scala/fx/HttpConnectionTimeoutSuite.scala
@@ -12,9 +12,18 @@ class HttpConnectionTimeoutSuite extends ScalaCheckSuite {
   property("Any positive long can be a HttpConnectionTimeout") {
     forAll { (i: Long) =>
       (i > 0) ==> {
-        HttpConnectionTimeout.of(i).right.get.toLong == i
+        run(HttpConnectionTimeout.of(i)) match {
+          case s: String => fail(s"unexpected shift to error: $s")
+          case _ => passed
+        }
       }
-      (i <= 0) ==> { HttpConnectionTimeout.of(i) == Left("Durations must be positive") }
+      (i <= 0) ==> {
+        run(HttpConnectionTimeout.of(i)) match {
+          case s: String => s == "Durations must be positive"
+          case _ =>
+            fail("0 or negative numbers should have failed")
+        }
+      }
     }
   }
 }
