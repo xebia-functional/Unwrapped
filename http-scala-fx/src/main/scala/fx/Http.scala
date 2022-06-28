be643486b78e5017bf1e4162d936d69b98b7ab7f
Jack Viers <jackcviers@gmail.com>
2022-06-28 09:06:42 -0500

    WIP: responsemappers

--- http-scala-fx/src/main/scala/fx/Http.scala
+++ http-scala-fx/src/main/scala/fx/Http.scala
@@ -5,28 +5,64 @@ import java.time.Duration
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse.BodyHandlers
 import java.net.http.HttpResponse
+import scala.annotation.tailrec
+import java.net.http.HttpHeaders
 
 /**
  * Models the http scheme
  */
-opaque type Http[A] = (HttpRetryPolicy, Structured, Control[HttpExecutionException], fx.HttpClient, HttpResponseMapper[A] ) ?=> A
+opaque type Http[A] = (
+    HttpRetryPolicy,
+    Structured,
+    Control[HttpExecutionException],
+    fx.HttpClient,
+    ) ?=> A
 
-object Http:
-  
-  def GET[A](uri: URI): Http[A] = {
+extension [A](a: Http[A])
+  @tailrec
+  def GET(uri: URI): HttpResponseMapper[A] ?=> Http[A] = {
     val mapper = summon[HttpResponseMapper[A]]
     val config = summon[HttpClientConfig]
     val retryPolicy = config.retryPolicy
     val retries = config.maximumRetries.getOrElse(summon[HttpRetries])
-    val response: HttpResponse[String] = summon[fx.HttpClient].value.send(HttpRequest.newBuilder().GET.uri(uri).build(), BodyHandlers.ofString())
-    if(response.shouldRetry){
+    val response = summon[fx.HttpClient]
+      .value
+      .send(HttpRequest.newBuilder().GET().uri(uri).build(), mapper.bodyHandler)
+    if (response.shouldRetry) {
       GET(uri)
-    } else if(response.statusCode() == 200){
-      response.toA
+    } else {
+      response.body
     }
   }
-  def HEAD(uri: URI): Http ?=> A = ???
-  def POST(uri: URI): HttpClientConfig ?=> A = ???
-  def PUT(uri: URI): HttpClientConfig ?=> A = ???
-  def PATCH(uri: URI): HttpClientConfig ?=> A = ???
 
+  def POST[B](uri: URI, body: B): (HttpResponseMapper[A], HttpBodyMapper[B]) ?=> Http[A] = {
+    val mapper = summon[HttpResponseMapper[A]]
+    val bodyMapper = summon[HttpBodyMapper[B]]
+    val config = summon[HttpClientConfig]
+    val retryPolicy = config.retryPolicy
+    val retries = config.maximumRetries.getOrElse(summon[HttpRetries])
+    val response = summon[fx.HttpClient]
+      .value
+      .send(HttpRequest.newBuilder().POST(bodyMapper.bodyPublisher(body)).uri(uri).build(), mapper.bodyHandler)
+    if (response.shouldRetry) {
+      GET(uri)
+    } else {
+      response.body
+    }
+  }
+
+  @tailrec
+  def HEAD(uri: URI): Http[HttpHeaders] = {
+    val config = summon[HttpClientConfig]
+    val retryPolicy = config.retryPolicy
+    val retries = config.maximumRetries.getOrElse(summon[HttpRetries])
+    val response = summon[fx.HttpClient]
+      .value
+      .send(HttpRequest.newBuilder().HEAD().uri(uri).build(), BodyHandlers.discarding)
+    if (response.shouldRetry) {
+      HEAD(uri)
+    } else response.headers
+  }
+  
+
+object Http
