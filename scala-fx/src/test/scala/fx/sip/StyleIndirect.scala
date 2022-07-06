a4b720df72ad9b6b73b894f3c6f494897eee3c90
raulraja <raulraja@gmail.com>
2022-07-06 18:24:59 +0200

    Progress with pre-sip draft

--- scala-fx/src/test/scala/fx/sip/StyleIndirect.scala
+++ scala-fx/src/test/scala/fx/sip/StyleIndirect.scala
@@ -3,13 +3,11 @@ package fx.sip
 import scala.concurrent.Future
 import concurrent.ExecutionContext.Implicits.global
 
-def getCountryCodeIndirect(maybePerson: Option[Person]): Future[Option[String]] =
-  maybePerson match
-    case Some(person) =>
-      person.address.map {
-        case Right(address) =>
-          address.country.flatMap(_.code)
-        case Left(_) =>
-          None
-      }
-    case None => Future.successful(None)
+def getCountryCodeIndirect(futurePerson: Future[Person]): Future[Option[String]] =
+  futurePerson.map { person =>
+    person.address match
+      case Right(address) =>
+        address.country.flatMap(_.code)
+      case Left(_) =>
+        None
+  }
