19c08d827f4500402bcbe046f80a37b5e0f01438
Pablo G. Thuillier <pablo.thuillier@47deg.com>
2022-07-11 16:11:06 +0200

    Use ScalaFXSuite instead of Scalacheck proprties

--- scalike-jdbc-scala-fx/src/test/scala/fx/DatabaseTests.scala
+++ scalike-jdbc-scala-fx/src/test/scala/fx/DatabaseTests.scala
@@ -1,41 +1,35 @@
 package fx
 
-import org.scalacheck.Properties
-import org.scalacheck.Prop.forAll
+import munit.fx.ScalaFXSuite
 import java.sql.SQLException
 import scalikejdbc.*
 import scalikejdbc.DB.*
 
-object DatabaseTests extends Properties("Database props"):
+class DatabaseTests extends ScalaFXSuite {
 
   Class.forName("org.postgresql.Driver")
   ConnectionPool.singleton("jdbc:postgresql://localhost:5432/scalafx", "postgres", "postgres")
 
-  property("Read only operation executes properly") = forAll { (s: String) =>
+  given DB.CPContext = NoCPContext
+  given SettingsProvider = SettingsProvider.default
 
-    given DB.CPContext = NoCPContext
-    given SettingsProvider = SettingsProvider.default
-
-    val names: Database[List[String]] = DB.readOnlyWithControl  { implicit session =>
+  testFX("Read only operation executes properly") {
+    val names: Database[List[String]] = DB.readOnlyWithControl { implicit session =>
       sql"select name from emp".map { rs => rs.string("name") }.list.apply()
     }
-    run(structured(names.join.toEither.isRight)) match {
+    assertFX(run(structured(names.join.toEither.isRight)) match {
       case b: Boolean => b
       case _ => false
-    }
+    })
   }
 
-  property("Read only shift when tries to execute a non-read only operation") = forAll { (s: String) =>
-
-    given DB.CPContext = NoCPContext
-    given SettingsProvider = SettingsProvider.default
-
-    val names: Database[Int] = DB.readOnlyWithControl  { implicit session =>
-      sql"update emp set name = ${s} where id = 1".update.apply()
+  testFX("Read only shift when tries to execute a non-read only operation") {
+    val names: Database[Int] = DB.readOnlyWithControl { implicit session =>
+      sql"update emp set name = 'Abc' where id = 1".update.apply()
     }
-    run(structured(names.join.toEither.isLeft)) match {
+    assertFX(run(structured(names.join.toEither.isLeft)) match {
       case b: Boolean => false
       case _ => true
-    }
+    })
   }
-end DatabaseTests
+}
