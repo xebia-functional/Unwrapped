7de2d97cc40227e7babbbe0ced7ddf5dd54c4ba6
Pablo G. Thuillier <pablo.thuillier@47deg.com>
2022-07-21 12:00:48 +0200

    Remove Fiber layer from db operations

--- scalike-jdbc-scala-fx/src/main/scala/fx/DatabaseScala.scala
+++ scalike-jdbc-scala-fx/src/main/scala/fx/DatabaseScala.scala
@@ -6,17 +6,17 @@ import scalikejdbc.DB.{readOnly, CPContext, NoCPContext}
 import java.sql.SQLException
 import fx.ensure
 
-type Database[A] = (Structured, Control[SQLException]) ?=> Fiber[A]
-type Transaction[A] = (Structured, Control[Exception]) ?=> Fiber[A]
+type Database[A] = Control[SQLException] ?=> A
+type Transaction[A] = Control[Exception] ?=> A
 
 extension (db: DB.type)
   def readOnlyWithControl[A](execution: DBSession => A)(
       using context: CPContext = NoCPContext,
       settings: SettingsProvider = SettingsProvider.default): Database[A] =
-    fork { () => handle(DB.readOnly(execution))((e: SQLException) => e.shift) }
+    handle(DB.readOnly(execution))((e: SQLException) => e.shift)
 
   def localTransaction[A](execution: DBSession => A)(
       using context: CPContext = NoCPContext,
       boundary: TxBoundary[A] = TxBoundary.Exception.exceptionTxBoundary[A],
       settings: SettingsProvider = SettingsProvider.default): Transaction[A] =
-    fork { () => handle(DB.localTx(execution))((e: Exception) => e.shift) }
+    handle(DB.localTx(execution))((e: Exception) => e.shift)
--- scalike-jdbc-scala-fx/src/test/scala/fx/DatabaseSpec.scala
+++ scalike-jdbc-scala-fx/src/test/scala/fx/DatabaseSpec.scala
@@ -7,40 +7,39 @@ import scalikejdbc.DB.*
 class DatabaseSpec extends DatabaseSuite {
 
   testFX("Read only operation executes properly") {
-    val names: Database[List[String]] = DB.readOnlyWithControl { implicit session =>
-      sql"select name from emp".map { rs => rs.string("name") }.list.apply()
+    val names: Database[List[String]] = DB.readOnlyWithControl {
+      case given DBSession =>
+        sql"select name from emp".map { rs => rs.string("name") }.list.apply()
     }
-    assertEqualsFX(structured(names.join), List("Ana", "Mike"))
+    assertEqualsFX(names, List("Ana", "Mike"))
   }
 
   testFX("Read only shift when tries to execute a non-read only operation") {
-    assertFX(toEither(structured(DB.readOnlyWithControl {
+    assertFX(toEither(DB.readOnlyWithControl {
       case given DBSession =>
         sql"update emp set name = 'Never changed in a readonly operation' where id = 1"
           .update
           .apply()
-    }).join).isLeft)
+    }).isLeft)
   }
 
   testFX("Transaction goes well after updating 2 rows.") {
-    val count: Transaction[Int] = DB.localTransaction { implicit session =>
-      sql"update emp set name = 'Abc' where id = 1".update.apply()
-      sql"update emp set name = 'Emp' where id = 2".update.apply()
+    val count: Transaction[Int] = DB.localTransaction {
+      case given DBSession =>
+        sql"update emp set name = 'Abc' where id = 1".update.apply()
+        sql"update emp set name = 'Emp' where id = 2".update.apply()
     }
-    assertEqualsFX(run(structured(count.join)), 1)
+    assertEqualsFX(count, 1)
   }
 
   testFX("Transaction goes wrong and rollback after second update raises an exception") {
-    val count: Transaction[Int] = DB.localTransaction { implicit session =>
-      sql"update emp set name = 'Never changed in a failed transaction' where id = 1"
-        .update
-        .apply()
-      sql"update emp set name = 'Empty' where inventing_row = 0".update.apply()
-    }
-    assertFX(run(structured(count.join.toEither.isLeft)) match {
-      case b: Boolean => false
-      case _ => true
-    })
+    assertFX(toEither(DB.localTransaction {
+      case given DBSession =>
+        sql"update emp set name = 'Never changed in a failed transaction' where id = 1"
+          .update
+          .apply()
+        sql"update emp set name = 'Empty' where inventing_row = 0".update.apply()
+    }).isLeft)
   }
 
 }
