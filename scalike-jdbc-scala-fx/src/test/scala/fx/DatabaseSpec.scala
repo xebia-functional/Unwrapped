0a13e3d205e9b696dbe4c4714c10d23b42a24f63
Pablo G. Thuillier <pgthuillier@gmail.com>
2022-07-21 10:58:38 +0200

    Apply suggestions from code review
    Use handle instead of try/catch and remove run call from testFX
    
    Co-authored-by: Jack Viers <jackcviers@gmail.com>

--- scalike-jdbc-scala-fx/src/test/scala/fx/DatabaseSpec.scala
+++ scalike-jdbc-scala-fx/src/test/scala/fx/DatabaseSpec.scala
@@ -10,22 +10,16 @@ class DatabaseSpec extends DatabaseSuite {
     val names: Database[List[String]] = DB.readOnlyWithControl { implicit session =>
       sql"select name from emp".map { rs => rs.string("name") }.list.apply()
     }
-    assertFX(run(structured(names.join.toEither.isRight)) match {
-      case b: Boolean => b
-      case _ => false
-    })
+    assertEqualsFX(structured(names.join), List("Ana", "Mike"))
   }
 
   testFX("Read only shift when tries to execute a non-read only operation") {
-    val names: Database[Int] = DB.readOnlyWithControl { implicit session =>
-      sql"update emp set name = 'Never changed in a readonly operation' where id = 1"
-        .update
-        .apply()
-    }
-    assertFX(run(structured(names.join.toEither.isLeft)) match {
-      case b: Boolean => false
-      case _ => true
-    })
+    assertFX(toEither(structured(DB.readOnlyWithControl {
+      case given DBSession =>
+        sql"update emp set name = 'Never changed in a readonly operation' where id = 1"
+          .update
+          .apply()
+    }).join).isLeft)
   }
 
   testFX("Transaction goes well after updating 2 rows.") {
@@ -36,7 +30,7 @@ class DatabaseSpec extends DatabaseSuite {
     assertEqualsFX(run(structured(count.join)), 1)
   }
 
-  testFX("Transaction goes wrong and rollback after second update raise an exception") {
+  testFX("Transaction goes wrong and rollback after second update raises an exception") {
     val count: Transaction[Int] = DB.localTransaction { implicit session =>
       sql"update emp set name = 'Never changed in a failed transaction' where id = 1"
         .update
