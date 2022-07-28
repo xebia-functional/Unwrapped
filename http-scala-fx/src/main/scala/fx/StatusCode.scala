548ef7c74cda4889b318a3f57ca0280ddf1c1ae3
Jack Viers <jackcviers@gmail.com>
2022-07-28 10:36:28 -0500

    READY for body from http client

--- http-scala-fx/src/main/scala/fx/StatusCode.scala
+++ http-scala-fx/src/main/scala/fx/StatusCode.scala
@@ -5,7 +5,10 @@ package fx
  */
 opaque type StatusCode = Int
 
+
 object StatusCode:
+  def value(s: StatusCode): Int = s
+
   def statusCodes =
     (100 to 103).toSet ++ (200 to 208).toSet + 226 ++ (300 to 308).toSet ++ (400 to 418).toSet ++ (421 to 426).toSet ++ Set(
       428,
@@ -40,4 +43,5 @@ object StatusCode:
   def unsafeOf(i: Int): StatusCode =
     assert(statusCodes.contains(i))
     i
-  extension (s: StatusCode) def value: Int = s
+  extension(s: StatusCode)
+    def statusCodeValue: Int = s
--- sttp-scala-fx/src/main/scala/sttp/fx/HttpScalaFXBackend.scala
+++ sttp-scala-fx/src/main/scala/sttp/fx/HttpScalaFXBackend.scala
@@ -16,6 +16,7 @@ import java.net.http.HttpResponse
 import scala.jdk.CollectionConverters.*
 import sttp.model.RequestMetadata
 import java.util.zip.Deflater
+import java.util.zip.Inflater
 
 class HttpScalaFXBackend(using client: HttpClient, control: Control[Throwable])
     extends SttpBackend[Http, Streams[Receive[Byte]] with Effect[Http]] {
@@ -36,9 +37,9 @@ class HttpScalaFXBackend(using client: HttpClient, control: Control[Throwable])
     if (method == Method.GET) {} else {}
     val httpResponse = method match {
       case Method.GET => {
-        val response = uri.toJavaUri.GET[Receive[Byte]](headers: _*)
-        if (response.status == OK.value) {
-          val byteStream = response.body()
+        val r = uri.toJavaUri.GET[Receive[Byte]](headers: _*)
+        if (r.statusCode() == OK.statusCodeValue) {
+          val byteStream = r.body()
           val inflater = new Inflater(false)
           Response(
             null,
@@ -46,10 +47,10 @@ class HttpScalaFXBackend(using client: HttpClient, control: Control[Throwable])
             byteStream
               .grouped(4096)
               .transform { bytes =>
-                val output = Array.empty[Byte]
-                inflater.setInput(bytes)
+                val output: Array[Byte] = Array.empty[Byte]
+                inflater.setInput(bytes.toArray)
                 inflater.inflate(output)
-                send(output)
+                _root_.fx.send(output)
                 inflater.reset()
               }
               .toList
@@ -67,8 +68,14 @@ class HttpScalaFXBackend(using client: HttpClient, control: Control[Throwable])
             null,
             sttp.model.StatusCode(r.statusCode()),
             "",
+            r.headers()
+              .map()
+              .asScala
+              .flatMap { kv => kv._2.asScala.map { headerValue => Header(kv._1, headerValue) } }
+              .toList,
             List.empty,
-            RequestMetadata(method, uri, request.headers))
+            RequestMetadata(method, uri, request.headers)
+          )
         }
       }
       case Method.HEAD =>
@@ -87,9 +94,9 @@ class HttpScalaFXBackend(using client: HttpClient, control: Control[Throwable])
           )
         }
       case Method.POST =>
-        val response = uri.toJavaUri.POST[Receive[Byte], String](body.show, headers: _*)
-        if (response.status == OK.value) {
-          val byteStream = response.body()
+        val r = uri.toJavaUri.POST[Receive[Byte], String](body.show, headers: _*)
+        if (r.statusCode() == OK.statusCodeValue) {
+          val byteStream = r.body()
           val inflater = new Inflater(false)
           Response(
             null,
@@ -98,9 +105,9 @@ class HttpScalaFXBackend(using client: HttpClient, control: Control[Throwable])
               .grouped(4096)
               .transform { bytes =>
                 val output = Array.empty[Byte]
-                inflater.setInput(bytes)
+                inflater.setInput(bytes.toArray)
                 inflater.inflate(output)
-                send(output)
+                _root_.fx.send(output)
                 inflater.reset()
               }
               .toList
@@ -118,29 +125,36 @@ class HttpScalaFXBackend(using client: HttpClient, control: Control[Throwable])
             null,
             sttp.model.StatusCode(r.statusCode()),
             "",
+            r.headers()
+              .map()
+              .asScala
+              .flatMap { kv => kv._2.asScala.map { headerValue => Header(kv._1, headerValue) } }
+              .toList,
             List.empty,
-            RequestMetadata(method, uri, request.headers))
+            RequestMetadata(method, uri, request.headers)
+          )
         }
       case Method.PUT => {
         val response = uri.toJavaUri.PUT[Receive[Byte], String](body.show, headers: _*)
-        if (response.status == OK.value) {
+        if (response.statusCode() == OK.statusCodeValue) {
           val byteStream = response.body()
           val inflater = new Inflater(false)
           Response(
             null,
-            sttp.model.StatusCode(r.statusCode()),
+            sttp.model.StatusCode(response.statusCode()),
             byteStream
               .grouped(4096)
               .transform { bytes =>
                 val output = Array.empty[Byte]
-                inflater.setInput(bytes)
+                inflater.setInput(bytes.toArray)
                 inflater.inflate(output)
-                send(output)
+                _root_.fx.send(output)
                 inflater.reset()
               }
               .toList
               .mkString,
-            r.headers()
+            response
+              .headers()
               .map()
               .asScala
               .flatMap { kv => kv._2.asScala.map { headerValue => Header(kv._1, headerValue) } }
@@ -151,32 +165,40 @@ class HttpScalaFXBackend(using client: HttpClient, control: Control[Throwable])
         } else {
           Response(
             null,
-            sttp.model.StatusCode(r.statusCode()),
+            sttp.model.StatusCode(response.statusCode()),
             "",
+            response
+              .headers()
+              .map()
+              .asScala
+              .flatMap { kv => kv._2.asScala.map { headerValue => Header(kv._1, headerValue) } }
+              .toList,
             List.empty,
-            RequestMetadata(method, uri, request.headers))
+            RequestMetadata(method, uri, request.headers)
+          )
         }
       }
       case Method.DELETE => {
         val response = uri.toJavaUri.DELETE[Receive[Byte]](headers: _*)
-        if (response.status == OK.value) {
+        if (response.statusCode() == OK.statusCodeValue) {
           val byteStream = response.body()
           val inflater = new Inflater(false)
           Response(
             null,
-            sttp.model.StatusCode(r.statusCode()),
+            sttp.model.StatusCode(response.statusCode()),
             byteStream
               .grouped(4096)
               .transform { bytes =>
                 val output = Array.empty[Byte]
-                inflater.setInput(bytes)
+                inflater.setInput(bytes.toArray)
                 inflater.inflate(output)
-                send(output)
+                _root_.fx.send(output)
                 inflater.reset()
               }
               .toList
               .mkString,
-            r.headers()
+            response
+              .headers()
               .map()
               .asScala
               .flatMap { kv => kv._2.asScala.map { headerValue => Header(kv._1, headerValue) } }
@@ -187,32 +209,40 @@ class HttpScalaFXBackend(using client: HttpClient, control: Control[Throwable])
         } else {
           Response(
             null,
-            sttp.model.StatusCode(r.statusCode()),
+            sttp.model.StatusCode(response.statusCode()),
             "",
+            response
+              .headers()
+              .map()
+              .asScala
+              .flatMap { kv => kv._2.asScala.map { headerValue => Header(kv._1, headerValue) } }
+              .toList,
             List.empty,
-            RequestMetadata(method, uri, request.headers))
+            RequestMetadata(method, uri, request.headers)
+          )
         }
       }
       case Method.OPTIONS => {
         val response = uri.toJavaUri.OPTIONS[Receive[Byte]](headers: _*)
-        if (response.status == OK.value) {
+        if (response.statusCode() == OK.statusCodeValue) {
           val byteStream = response.body()
           val inflater = new Inflater(false)
           Response(
             null,
-            sttp.model.StatusCode(r.statusCode()),
+            sttp.model.StatusCode(response.statusCode()),
             byteStream
               .grouped(4096)
               .transform { bytes =>
                 val output = Array.empty[Byte]
-                inflater.setInput(bytes)
+                inflater.setInput(bytes.toArray)
                 inflater.inflate(output)
-                send(output)
+                _root_.fx.send(output)
                 inflater.reset()
               }
               .toList
               .mkString,
-            r.headers()
+            response
+              .headers()
               .map()
               .asScala
               .flatMap { kv => kv._2.asScala.map { headerValue => Header(kv._1, headerValue) } }
@@ -223,32 +253,40 @@ class HttpScalaFXBackend(using client: HttpClient, control: Control[Throwable])
         } else {
           Response(
             null,
-            sttp.model.StatusCode(r.statusCode()),
+            sttp.model.StatusCode(response.statusCode()),
             "",
+            response
+              .headers()
+              .map()
+              .asScala
+              .flatMap { kv => kv._2.asScala.map { headerValue => Header(kv._1, headerValue) } }
+              .toList,
             List.empty,
-            RequestMetadata(method, uri, request.headers))
+            RequestMetadata(method, uri, request.headers)
+          )
         }
       }
       case Method.PATCH => {
         val response = uri.toJavaUri.PATCH[Receive[Byte], String](body.show, headers: _*)
-        if (response.status == OK.value) {
+        if (response.statusCode() == OK.statusCodeValue) {
           val byteStream = response.body()
           val inflater = new Inflater(false)
           Response(
             null,
-            sttp.model.StatusCode(r.statusCode()),
+            sttp.model.StatusCode(response.statusCode()),
             byteStream
               .grouped(4096)
               .transform { bytes =>
                 val output = Array.empty[Byte]
-                inflater.setInput(bytes)
+                inflater.setInput(bytes.toArray)
                 inflater.inflate(output)
-                send(output)
+                _root_.fx.send(output)
                 inflater.reset()
               }
               .toList
               .mkString,
-            r.headers()
+            response
+              .headers()
               .map()
               .asScala
               .flatMap { kv => kv._2.asScala.map { headerValue => Header(kv._1, headerValue) } }
@@ -259,32 +297,40 @@ class HttpScalaFXBackend(using client: HttpClient, control: Control[Throwable])
         } else {
           Response(
             null,
-            sttp.model.StatusCode(r.statusCode()),
+            sttp.model.StatusCode(response.statusCode()),
             "",
+            response
+              .headers()
+              .map()
+              .asScala
+              .flatMap { kv => kv._2.asScala.map { headerValue => Header(kv._1, headerValue) } }
+              .toList,
             List.empty,
-            RequestMetadata(method, uri, request.headers))
+            RequestMetadata(method, uri, request.headers)
+          )
         }
       }
       case Method.TRACE => {
         val response = uri.toJavaUri.TRACE[Receive[Byte]](headers: _*)
-        if (response.status == OK.value) {
+        if (response.statusCode() == OK.statusCodeValue) {
           val byteStream = response.body()
           val inflater = new Inflater(false)
           Response(
             null,
-            sttp.model.StatusCode(r.statusCode()),
+            sttp.model.StatusCode(response.statusCode()),
             byteStream
               .grouped(4096)
               .transform { bytes =>
                 val output = Array.empty[Byte]
-                inflater.setInput(bytes)
+                inflater.setInput(bytes.toArray)
                 inflater.inflate(output)
-                send(output)
+                _root_.fx.send(output)
                 inflater.reset()
               }
               .toList
               .mkString,
-            r.headers()
+            response
+              .headers()
               .map()
               .asScala
               .flatMap { kv => kv._2.asScala.map { headerValue => Header(kv._1, headerValue) } }
@@ -295,10 +341,17 @@ class HttpScalaFXBackend(using client: HttpClient, control: Control[Throwable])
         } else {
           Response(
             null,
-            sttp.model.StatusCode(r.statusCode()),
+            sttp.model.StatusCode(response.statusCode()),
             "",
+            response
+              .headers()
+              .map()
+              .asScala
+              .flatMap { kv => kv._2.asScala.map { headerValue => Header(kv._1, headerValue) } }
+              .toList,
             List.empty,
-            RequestMetadata(method, uri, request.headers))
+            RequestMetadata(method, uri, request.headers)
+          )
         }
       }
       case Method.CONNECT => ???
