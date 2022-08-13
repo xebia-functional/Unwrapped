5d93bce50ea71eb36fe2e58e1037af8f4f1343b2
Jack Viers <jackcviers@gmail.com>
2022-08-13 12:31:23 -0500

    WIP: Implements send and makerequest

--- sttp-scala-fx/src/main/scala/sttp/fx/HttpScalaFXBackend.scala
+++ sttp-scala-fx/src/main/scala/sttp/fx/HttpScalaFXBackend.scala
@@ -8,6 +8,10 @@ import sttp.capabilities.Effect
 import sttp.client3.Request
 import sttp.client3.Response
 import sttp.client3.RequestBody
+import sttp.model.Header
+import sttp.model.Method
+import java.net.URI
+import java.net.{http => jnh}
 
 class HttpScalaFXBackend(
     using client: HttpClient,
@@ -17,17 +21,50 @@ class HttpScalaFXBackend(
   given MonadError[Http] = responseMonad
 
   def requestAsBody[R, A <: RequestBody[R]](r: A)(
-      using ToHttpBodyMapper[A]): Http[HttpBodyMapper[A]] =
+    using ToHttpBodyMapper[A]): Http[HttpBodyMapper[A]] =
+    
     responseMonad.handleError(r.toHttpBodyMapper()) { case e: Throwable => e.shift }
 
-  def send[T, R >: ReceiveStreams with Effect[Http]](
-    request: Request[T, R]): Http[Response[T]] =
-    val bodyMapper: HttpBodyMapper[RequestBody[R]] = requestAsBody(request.body).httpValue
-    request.method match {
-      //map to request and do bodyFromResponseAs
-      case _ => ???
-    }
+  def getHeaders[T, R >: ReceiveStreams with Effect[Http]](request: Request[T, R])(
+      using bodyMapper: HttpBodyMapper[RequestBody[R]]): Http[::[HttpHeader]] = {
+    val `Content-Type`: HttpHeader = HttpHeader(
+      ("Content-Type" -> ::[String](bodyMapper.mediaType.value, List.empty[String])))
+    ::(`Content-Type`, request.headers.groupBy(_.name).map { (p: (String, Seq[Header])) =>
+      val values = p._2.map(_.value).toList
+      HttpHeader(p._1, values.headOption.getOrElse(""), values.tail: _*)
+    }.toList)
+  }
+
+  def toResponse[T, R >: ReceiveStreams with Effect[Http], A](response: jnh.HttpResponse[A]): Http[Response[T]] = {
     ???
+  }
+
+  def getUri[T, R >: ReceiveStreams with Effect[Http]](request: Request[T, R]): Http[URI] = {
+    handle(request.uri.toJavaUri){ e => HttpExecutionException(e).shift}
+  }
+
+
+  def makeRequest[T, R >: ReceiveStreams with Effect[Http]](
+      request: Request[T, R])(using HttpBodyMapper[RequestBody[R]], HttpResponseMapper[T]): Http[Response[T]] = {
+    val headers: ::[HttpHeader] = getHeaders(request)
+    val uri: URI = getUri(request).httpValue
+    request.method match {
+      case Method.CONNECT => handle(???){e => HttpExecutionException(e).shift}
+      case Method.DELETE => toResponse(uri.DELETE[T](headers: _*))
+      case Method.GET => toResponse(uri.GET[T](headers: _*))
+      case Method.HEAD => toResponse(uri.HEAD(headers: _*))
+      case Method.OPTIONS => toResponse(uri.OPTIONS(headers: _*))
+      case Method.PATCH => toResponse(uri.PATCH(request.body, headers: _*))
+      case Method.POST => toResponse(uri.POST(request.body, headers: _*))
+      case Method.PUT => toResponse(uri.PUT(request.body, headers: _*))
+      case Method.TRACE => toResponse(uri.TRACE(headers: _*))
+    }
+  }
+
+  def send[T, R >: ReceiveStreams with Effect[Http]](
+      request: Request[T, R])(using HttpResponseMapper[T]): Http[Response[T]] =
+    given bodyMapper: HttpBodyMapper[RequestBody[R]] = requestAsBody(request.body).httpValue
+    makeRequest(request)
 
   def close(): Http[Unit] = ???
 
