60dde5f154f21027b120ef114fccd8d024c6a5c0
Jack Viers <jackcviers@gmail.com>
2022-09-02 13:50:11 -0500

    Fixes things that somehow didn't get committed

--- http-scala-fx/src/main/scala/fx/Http.scala
+++ http-scala-fx/src/main/scala/fx/Http.scala
@@ -39,24 +39,22 @@ extension (uri: URI)
     }
 
   def GET[A](): HttpResponseMapper[A] ?=> Http[HttpResponse[A]] =
-    GET(0, None, List.empty: _*)
+    GET(HttpRetries(0), None, List.empty: _*)
 
   def GET[A](headers: HttpHeader*): HttpResponseMapper[A] ?=> Http[HttpResponse[A]] =
-    GET(0, None, headers: _*)
+    GET(HttpRetries(0), None, headers: _*)
 
   def GET[A](
       timeout: ScalaDuration,
       headers: HttpHeader*): HttpResponseMapper[A] ?=> Http[HttpResponse[A]] =
-    GET(0, Option(timeout), headers: _*)
+    GET(HttpRetries(0), Option(timeout), headers: _*)
 
   @tailrec
   private def GET[A](
-      retryCount: Int,
+      retryCount: HttpRetries,
       maybeTimeout: Option[ScalaDuration],
       headers: HttpHeader*): HttpResponseMapper[A] ?=> Http[HttpResponse[A]] =
     val mapper = summon[HttpResponseMapper[A]]
-    val config = summon[HttpClientConfig]
-    val retries = config.maximumRetries.getOrElse(summon[HttpRetries])
     val requestBuilder =
       addHeadersToRequestBuilder(HttpRequest.newBuilder().GET().uri(uri), headers: _*)
 
@@ -73,30 +71,28 @@ extension (uri: URI)
             mapper.bodyHandler)) { ex => HttpExecutionException(ex).shift }
 
     if (response.shouldRetry(retryCount)) {
-      GET(retryCount + 1, maybeTimeout, headers: _*)
+      GET(retryCount + HttpRetries(1), maybeTimeout, headers: _*)
     } else {
       response
     }
 
   def POST[A, B](body: B, headers: HttpHeader*)
       : (HttpResponseMapper[A], HttpBodyMapper[B]) ?=> Http[HttpResponse[A]] =
-    POST(body, None, 0, headers: _*)
+    POST(body, None, HttpRetries(0), headers: _*)
 
   def POST[A, B](body: B, timeout: ScalaDuration, headers: HttpHeader*)
       : (HttpResponseMapper[A], HttpBodyMapper[B]) ?=> Http[HttpResponse[A]] =
-    POST(body, Option(timeout), 0, headers: _*)
+    POST(body, Option(timeout), HttpRetries(0), headers: _*)
 
   @tailrec
   private def POST[A, B](
       body: B,
       maybeTimeout: Option[ScalaDuration],
-      retryCount: Int = 0,
+      retryCount: HttpRetries,
       headers: HttpHeader*)
       : (HttpResponseMapper[A], HttpBodyMapper[B]) ?=> Http[HttpResponse[A]] =
     val mapper = summon[HttpResponseMapper[A]]
     val bodyMapper = summon[HttpBodyMapper[B]]
-    val config = summon[HttpClientConfig]
-    val retries = config.maximumRetries.getOrElse(summon[HttpRetries])
     val requestBuilder = addHeadersToRequestBuilder(
       HttpRequest.newBuilder().POST(bodyMapper.bodyPublisher(body)).uri(uri),
       headers: _*)
@@ -113,22 +109,21 @@ extension (uri: URI)
               .build(),
             mapper.bodyHandler)) { ex => HttpExecutionException(ex).shift }
     if (response.shouldRetry(retryCount)) {
-      POST(body, maybeTimeout, retryCount + 1, headers: _*)
+      POST(body, maybeTimeout, retryCount + HttpRetries(1), headers: _*)
     } else {
       response
     }
 
-  def HEAD(headers: HttpHeader*): Http[HttpResponse[Void]] = HEAD(None, 0, headers: _*)
+  def HEAD(headers: HttpHeader*): Http[HttpResponse[Void]] =
+    HEAD(None, HttpRetries(0), headers: _*)
   def HEAD(timeout: ScalaDuration, headers: HttpHeader*): Http[HttpResponse[Void]] =
-    HEAD(Option(timeout), 0, headers: _*)
+    HEAD(Option(timeout), HttpRetries(0), headers: _*)
 
   @tailrec
   private def HEAD(
       maybeTimeout: Option[ScalaDuration],
-      retryCount: Int = 0,
+      retryCount: HttpRetries,
       headers: HttpHeader*): Http[HttpResponse[Void]] =
-    val config = summon[HttpClientConfig]
-    val retries = config.maximumRetries.getOrElse(summon[HttpRetries])
     val requestBuilder =
       addHeadersToRequestBuilder(HttpRequest.newBuilder().HEAD().uri(uri), headers: _*)
     val response =
@@ -143,28 +138,26 @@ extension (uri: URI)
               .build(),
             BodyHandlers.discarding)) { ex => HttpExecutionException(ex).shift }
     if (response.shouldRetry(retryCount)) {
-      HEAD(maybeTimeout, retryCount + 1, headers: _*)
+      HEAD(maybeTimeout, retryCount + HttpRetries(1), headers: _*)
     } else response
 
   def PUT[A, B](body: B, headers: HttpHeader*)
       : (HttpResponseMapper[A], HttpBodyMapper[B]) ?=> Http[HttpResponse[A]] =
-    PUT[A, B](body, None, 0, headers: _*)
+    PUT[A, B](body, None, HttpRetries(0), headers: _*)
 
   def PUT[A, B](body: B, timeout: ScalaDuration, headers: HttpHeader*)
       : (HttpResponseMapper[A], HttpBodyMapper[B]) ?=> Http[HttpResponse[A]] =
-    PUT[A, B](body, Option(timeout), 0, headers: _*)
+    PUT[A, B](body, Option(timeout), HttpRetries(0), headers: _*)
 
   @tailrec
   private def PUT[A, B](
       body: B,
       maybeTimeout: Option[ScalaDuration],
-      retryCount: Int = 0,
+      retryCount: HttpRetries,
       headers: HttpHeader*)
       : (HttpResponseMapper[A], HttpBodyMapper[B]) ?=> Http[HttpResponse[A]] =
     val mapper = summon[HttpResponseMapper[A]]
     val bodyMapper = summon[HttpBodyMapper[B]]
-    val config = summon[HttpClientConfig]
-    val retries = config.maximumRetries.getOrElse(summon[HttpRetries])
     val requestBuilder = addHeadersToRequestBuilder(
       HttpRequest.newBuilder().PUT(bodyMapper.bodyPublisher(body)).uri(uri),
       headers: _*)
@@ -180,24 +173,22 @@ extension (uri: URI)
               .build(),
             mapper.bodyHandler)) { ex => HttpExecutionException(ex).shift }
     if (response.shouldRetry(retryCount)) {
-      PUT(body, maybeTimeout, retryCount + 1, headers: _*)
+      PUT(body, maybeTimeout, retryCount + HttpRetries(1), headers: _*)
     } else response
 
   def DELETE[A](headers: HttpHeader*): (HttpResponseMapper[A]) ?=> Http[HttpResponse[A]] =
-    DELETE[A](None, 0, headers: _*)
+    DELETE[A](None, HttpRetries(0), headers: _*)
   def DELETE[A](
       timeout: ScalaDuration,
       headers: HttpHeader*): (HttpResponseMapper[A]) ?=> Http[HttpResponse[A]] =
-    DELETE[A](Option(timeout), 0, headers: _*)
+    DELETE[A](Option(timeout), HttpRetries(0), headers: _*)
 
   @tailrec
   private def DELETE[A](
       maybeTimeout: Option[ScalaDuration],
-      retryCount: Int = 0,
+      retryCount: HttpRetries,
       headers: HttpHeader*): (HttpResponseMapper[A]) ?=> Http[HttpResponse[A]] =
     val mapper = summon[HttpResponseMapper[A]]
-    val config = summon[HttpClientConfig]
-    val retries = config.maximumRetries.getOrElse(summon[HttpRetries])
     val requestBuilder =
       addHeadersToRequestBuilder(HttpRequest.newBuilder().DELETE().uri(uri), headers: _*)
     val response =
@@ -212,27 +203,25 @@ extension (uri: URI)
               .build(),
             mapper.bodyHandler)) { ex => HttpExecutionException(ex).shift }
     if (response.shouldRetry(retryCount)) {
-      DELETE(maybeTimeout, retryCount + 1, headers: _*)
+      DELETE(maybeTimeout, retryCount + HttpRetries(1), headers: _*)
     } else {
       response
     }
 
   def OPTIONS[A](headers: HttpHeader*): (HttpResponseMapper[A]) ?=> Http[HttpResponse[A]] =
-    OPTIONS(None, 0, headers: _*)
+    OPTIONS(None, HttpRetries(0), headers: _*)
 
   def OPTIONS[A](
       timeout: ScalaDuration,
       headers: HttpHeader*): (HttpResponseMapper[A]) ?=> Http[HttpResponse[A]] =
-    OPTIONS(Option(timeout), 0, headers: _*)
+    OPTIONS(Option(timeout), HttpRetries(0), headers: _*)
 
   @tailrec
   private def OPTIONS[A](
       maybeTimeout: Option[ScalaDuration],
-      retryCount: Int = 0,
+      retryCount: HttpRetries,
       headers: HttpHeader*): (HttpResponseMapper[A]) ?=> Http[HttpResponse[A]] =
     val mapper = summon[HttpResponseMapper[A]]
-    val config = summon[HttpClientConfig]
-    val retries = config.maximumRetries.getOrElse(summon[HttpRetries])
     val requestBuilder = addHeadersToRequestBuilder(
       HttpRequest.newBuilder().method("OPTIONS", BodyPublishers.noBody).uri(uri))
     val response =
@@ -247,25 +236,23 @@ extension (uri: URI)
               .build(),
             mapper.bodyHandler)) { ex => HttpExecutionException(ex).shift }
     if (response.shouldRetry(retryCount)) {
-      OPTIONS(maybeTimeout, retryCount + 1, headers: _*)
+      OPTIONS(maybeTimeout, retryCount + HttpRetries(1), headers: _*)
     } else response
 
   def TRACE[A](headers: HttpHeader*): HttpResponseMapper[A] ?=> Http[HttpResponse[A]] =
-    TRACE[A](None, 0, headers: _*)
+    TRACE[A](None, HttpRetries(0), headers: _*)
 
   def TRACE[A](
       timeout: ScalaDuration,
       headers: HttpHeader*): HttpResponseMapper[A] ?=> Http[HttpResponse[A]] =
-    TRACE[A](Option(timeout), 0, headers: _*)
+    TRACE[A](Option(timeout), HttpRetries(0), headers: _*)
 
   @tailrec
   private def TRACE[A](
       maybeTimeout: Option[ScalaDuration],
-      retryCount: Int = 0,
+      retryCount: HttpRetries,
       headers: HttpHeader*): HttpResponseMapper[A] ?=> Http[HttpResponse[A]] =
     val mapper = summon[HttpResponseMapper[A]]
-    val config = summon[HttpClientConfig]
-    val retries = config.maximumRetries.getOrElse(summon[HttpRetries])
     val requestBuilder = addHeadersToRequestBuilder(
       HttpRequest.newBuilder().method("TRACE", BodyPublishers.noBody).uri(uri),
       headers: _*)
@@ -281,26 +268,24 @@ extension (uri: URI)
               .build(),
             mapper.bodyHandler)) { ex => HttpExecutionException(ex).shift }
     if (response.shouldRetry(retryCount)) {
-      TRACE(maybeTimeout, retryCount + 1, headers: _*)
+      TRACE(maybeTimeout, retryCount + HttpRetries(1), headers: _*)
     } else response
 
   def PATCH[A, B](body: B, headers: HttpHeader*)
       : (HttpResponseMapper[A], HttpBodyMapper[B]) ?=> Http[HttpResponse[A]] =
-    PATCH(body, None, 0, headers: _*)
+    PATCH(body, None, HttpRetries(0), headers: _*)
   def PATCH[A, B](body: B, timeout: ScalaDuration, headers: HttpHeader*)
       : (HttpResponseMapper[A], HttpBodyMapper[B]) ?=> Http[HttpResponse[A]] =
-    PATCH(body, Option(timeout), 0, headers: _*)
+    PATCH(body, Option(timeout), HttpRetries(0), headers: _*)
 
   @tailrec
   private def PATCH[A, B](
       body: B,
       maybeTimeout: Option[ScalaDuration],
-      retryCount: Int = 0,
+      retryCount: HttpRetries,
       headers: HttpHeader*)
       : (HttpResponseMapper[A], HttpBodyMapper[B]) ?=> Http[HttpResponse[A]] =
     val mapper = summon[HttpResponseMapper[A]]
-    val config = summon[HttpClientConfig]
-    val retries = config.maximumRetries.getOrElse(summon[HttpRetries])
     val requestBuilder = addHeadersToRequestBuilder(
       HttpRequest
         .newBuilder()
@@ -320,7 +305,7 @@ extension (uri: URI)
             mapper.bodyHandler
           )) { ex => HttpExecutionException(ex).shift }
     if (response.shouldRetry(retryCount)) {
-      PATCH(body, maybeTimeout, retryCount + 1, headers: _*)
+      PATCH(body, maybeTimeout, retryCount + HttpRetries(1), headers: _*)
     } else response
 
 object Http:
