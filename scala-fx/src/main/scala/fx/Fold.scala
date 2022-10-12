e6e3294b31e45ed3bcea0cd778ad9340de1d2696
raulraja <raulraja@gmail.com>
2022-10-12 21:05:45 +0200

    fix extension def conflicts

--- scala-fx/src/main/scala/fx/Fold.scala
+++ scala-fx/src/main/scala/fx/Fold.scala
@@ -2,50 +2,52 @@ package fx
 
 import java.util.concurrent.CancellationException
 import scala.util.control.NonFatal
+import fx.Raise
 
 type Effect[R, A] = Raise[R] /* { suspend } */ ?=> A
 
-/**
- * `invoke` the [Effect] and [fold] the result:
- *   - _success_ [transform] result of [A] to a value of [B].
- *   - _raised_ [recover] from `raised` value of [R] to a value of [B].
- *   - _exception_ [error] from [Throwable] by transforming value into [B].
- *
- * This method should never be wrapped in `try`/`catch` as it will not throw any unexpected
- * errors, it will only result in [CancellationException], or fatal exceptions such as
- * `OutOfMemoryError`.
- */
-inline def fold[R, A, B](
-    program: Effect[R, A])(recover: (raised: R) => B, transform: (value: A) => B): B =
-  fold(program, throw _, recover, transform)
+object Fold:
+  /**
+   * `invoke` the [Effect] and [fold] the result:
+   *   - _success_ [transform] result of [A] to a value of [B].
+   *   - _raised_ [recover] from `raised` value of [R] to a value of [B].
+   *   - _exception_ [error] from [Throwable] by transforming value into [B].
+   *
+   * This method should never be wrapped in `try`/`catch` as it will not throw any unexpected
+   * errors, it will only result in [CancellationException], or fatal exceptions such as
+   * `OutOfMemoryError`.
+   */
+  inline def fold[R, A, B](
+      program: Effect[R, A])(recover: (raised: R) => B, transform: (value: A) => B): B =
+    fold(program, throw _, recover, transform)
 
-inline def fold[R, A, B](
-    program: Effect[R, A],
-    error: (error: Throwable) => B,
-    recover: (raised: R) => B,
-    transform: (value: A) => B
-): B =
-  val raise = DefaultRaise[R]()
-  try transform(program(using raise))
-  catch
-    case e: CancellationException =>
-      recover(raisedOrRethrow(e, raise))
-    case NonFatal(e) =>
-      error(e)
+  inline def fold[R, A, B](
+      program: Effect[R, A],
+      error: (error: Throwable) => B,
+      recover: (raised: R) => B,
+      transform: (value: A) => B
+  ): B =
+    val raise = DefaultRaise[R]()
+    try transform(program(using raise))
+    catch
+      case e: CancellationException =>
+        recover(raisedOrRethrow(e, raise))
+      case NonFatal(e) =>
+        error(e)
 
-/**
- * Returns the shifted value, rethrows the CancellationException if not our scope
- */
-private def raisedOrRethrow[R](exc: CancellationException, raise: DefaultRaise[R]): R =
-  exc match
-    case e: RaiseCancellationException[_] if raise.eq(e.raise) => e._raised.asInstanceOf[R]
-    case _ => throw exc
+  /**
+   * Returns the shifted value, rethrows the CancellationException if not our scope
+   */
+  private def raisedOrRethrow[R](exc: CancellationException, raise: DefaultRaise[R]): R =
+    exc match
+      case e: RaiseCancellationException[_] if raise.eq(e.raise) => e._raised.asInstanceOf[R]
+      case _ => throw exc
 
-/**
- * Serves as both purposes of a scope-reference token, and a default implementation for Raise.
- */
-private class DefaultRaise[R] extends Raise[R]:
-  extension (r: R) def raise[A]: A = throw RaiseCancellationException(r, this)
+  /**
+   * Serves as both purposes of a scope-reference token, and a default implementation for Raise.
+   */
+  class DefaultRaise[R] extends Raise[R]:
+    extension (r: R) def raise[A]: A = throw RaiseCancellationException(r, this)
 
-private class RaiseCancellationException[R](val _raised: Any | Null, val raise: Raise[R])
-    extends CancellationException("Raised Continuation")
+  private class RaiseCancellationException[R](val _raised: Any | Null, val raise: Raise[R])
+      extends CancellationException("Raised Continuation")
--- scala-fx/src/main/scala/fx/Runtime.scala
+++ scala-fx/src/main/scala/fx/Runtime.scala
@@ -5,9 +5,9 @@ import scala.annotation.implicitNotFound
 extension [R, A](c: Raise[R] ?=> A)
 
   def toEither: Either[R, A] =
-    fold(c)(Left(_), Right(_))
+    Fold.fold(c)(Left(_), Right(_))
 
   def toOption: Option[A] =
-    fold(c)(_ => None, Some(_))
+    Fold.fold(c)(_ => None, Some(_))
 
-  def run: (R | A) = fold(c)(identity, identity)
+  def run: (R | A) = Fold.fold(c)(identity, identity)
