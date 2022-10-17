0df5ce0fe46a88420b21fef58acd4a12b23f27cc
Jack C. Viers <jackcviers@gmail.com>
2022-10-16 23:29:20 -0500

    WIP - As far as I got

--- cats-scalafx/src/test/scala/StructurredFLaws.scala
+++ cats-scalafx/src/test/scala/StructurredFLaws.scala
@@ -4,6 +4,7 @@ import munit.fx.DisciplineFXSuite
 import _root_.cats.effect.laws.AsyncLaws
 import _root_.fx.instances.{FxAsync, StructuredF}
 import _root_.fx.Structured
+import _root_.cats.kernel.Eq
 import _root_.fx.structured
 import _root_.cats.implicits._
 import _root_.cats.effect.laws.AsyncTests
@@ -11,32 +12,71 @@ import _root_.cats.effect.implicits._
 import org.scalacheck.{Gen}
 import _root_.cats.effect.laws.AsyncTests
 import _root_.cats.effect.kernel.instances.*
+import _root_.cats.effect.kernel.testkit.*
 import _root_.cats.effect.kernel.testkit.SyncTypeGenerators.arbitrarySyncType
 import _root_.cats.laws.discipline.arbitrary._
 import fx.Structured
 import org.scalacheck.{
-Arbitrary => Arb
+  Arbitrary => Arb,
+  Cogen
 }
 import _root_.cats.effect.IO
 import java.util.concurrent.Executors
 import scala.concurrent.ExecutionContext
+import org.scalacheck.rng.Seed
+import _root_.cats.effect.kernel.testkit.OutcomeGenerators
+import _root_.cats.effect.kernel.Outcome
+import _root_.cats.kernel.Order
+import scala.concurrent.duration.FiniteDuration
+
 
 class StructurredFLaws extends DisciplineFXSuite {
 
   import FxAsync.{given, *}
 
-  given arbExecutionContext(using Resources, resource:Arb[Resource[ExecutionContext]]): Arb[Resource[ExecutionContext]] =
-    val shutdown: (ExecutionContext, ExitCase) => Unit = (ec, exitCase) => ec.shutdown()
+  def seedOf[A](seed: Seed, fa: StructuredF[A]): Seed = seed
+
+  given arbCogen: Cogen[StructuredF[Int]] = Cogen[StructuredF[Int]](seedOf[Int](_, _))
+
+  given arbCogenOutcome: Cogen[Outcome[StructuredF, Throwable, Int]] = OutcomeGenerators.cogenOutcome[StructuredF, Throwable, Int]
+
+  given arbitraryExecutionContext: Arb[ExecutionContext] = Arb{
     for {
       threads <- Gen.choose(1, Runtime.getRuntime.availableProcessors - 1)
-      rec <- Gen.oneOf(
-        Resource(ExecutionContext.fromExecutorService(Executors.newCachedThreadPool()), shutdown),
-        Resource(ExecutionContext.fromExecutorService(Executors.newVirtualThreadPerTaskExecutor()), shutdown),
-        Resource(ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(threads)), shutdown),
-        Resource(ExecutionContext.fromExecutorService(Executors.newWorkStealingPool()), shutdown)
+      ec <- Gen.oneOf(
+        ExecutionContext.fromExecutorService(Executors.newCachedThreadPool()),
+        ExecutionContext.fromExecutorService(Executors.newVirtualThreadPerTaskExecutor()),
+        ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(threads)),
+        ExecutionContext.fromExecutorService(Executors.newWorkStealingPool()),
       )
+      _ = sys.addShutdownHook(ec.shutdown())
+    } yield ec
+  }
 
-    } yield rec
+  given eqF[A:Eq](using struct: Structured): Eq[StructuredF[A]] = new Eq{
+    def eqv(x: StructuredF[A], y: StructuredF[A]): Boolean =
+      val xval: A | Any = run{ x }
+      val yval: A | Any = run{ y }
+      xval == yval
+  }
+  given eqF(using struct: Structured): Eq[Throwable] = new Eq{
+    def eqv(x: Throwable, y: Throwable): Boolean =
+      x.getClass == y.getClass && x.getMessage == y.getMessage
+  }
+  given eqEc(using struct: Structured): Eq[ExecutionContext] = new Eq{
+    def eqv(x: ExecutionContext, y: ExecutionContext): Boolean =
+      x === y
+  }
+
+  given orderStrucuteredFFiniteDuration(using Structured, Order[FiniteDuration]): Order[StructuredF[FiniteDuration]] = new Order[StructuredF[FiniteDuration]] {
+    def compare(x: Order[StructuredF[FiniteDuration]], y: Order[StructuredF[FiniteDuration]]): Int =
+      val x: FiniteDuration | Any = run{x}
+      val y: FiniteDuration | Any = run{y}
+      (x, y) match {
+        case (xf: FiniteDuration, yf: FiniteDuration) => Order[FiniteDuration].compare(xf, yf)
+        case _ => 0
+      }
+  }
   
   given arbInt[A](using arbA: Arb[A], struct: Structured): Arb[StructuredF[A]] = Arb.apply{
     
