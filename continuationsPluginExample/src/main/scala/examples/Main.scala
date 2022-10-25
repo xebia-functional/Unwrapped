90fc94e4c8424fb33a3262300f3006570d7b6c92
Pablo Conde de la Mata <pablo.conde@47deg.com>
2022-10-25 10:37:54 +0200

    Fix after merge

--- continuationsPluginExample/src/main/scala/examples/Main.scala
+++ continuationsPluginExample/src/main/scala/examples/Main.scala
@@ -17,8 +17,7 @@ final class main$continuation$1($completion: Continuation[Any | Null])
   var label: Int = 0
 
   final def invokeSuspend(
-      result: Either[Throwable, Any | Null | (continuations.Continuation.State.Suspended.type)])
-      : Any | Null =
+      result: Either[Throwable, Any | Null | Continuation.State.Suspended.type]): Any | Null =
     this.result = result
     this.label |= Integer.MIN_VALUE
     main$expanded(using this)
