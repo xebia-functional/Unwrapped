0059249213108ae1b82a36153ffb7106e28eff2b
Pablo Conde de la Mata <pablo.conde@47deg.com>
2022-11-14 15:33:52 +0100

    Detect suspendion points for context fun and using

--- continuationsPlugin/src/main/scala/continuations/ContinuationsPlugin.scala
+++ continuationsPlugin/src/main/scala/continuations/ContinuationsPlugin.scala
@@ -2,16 +2,17 @@ package continuations
 
 import dotty.tools.dotc.report
 import dotty.tools.dotc.ast.Trees.*
-import dotty.tools.dotc.ast.tpd
+import dotty.tools.dotc.ast.{tpd, Trees}
 import dotty.tools.dotc.core.Constants.Constant
-import dotty.tools.dotc.core.Contexts.Context
+import dotty.tools.dotc.core.Contexts.{atPhase, Context}
 import dotty.tools.dotc.core.Decorators.*
 import dotty.tools.dotc.core.StdNames.*
-import dotty.tools.dotc.core.Symbols.*
+import dotty.tools.dotc.core.Symbols
 import dotty.tools.dotc.core.Types.{AppliedType, Type}
 import dotty.tools.dotc.plugins.{PluginPhase, StandardPlugin}
 import dotty.tools.dotc.semanticdb.TypeMessage.SealedValue.TypeRef
 import dotty.tools.dotc.transform.{PickleQuotes, Staging}
+import dotty.tools.dotc.core.{Flags, NameKinds}
 
 import scala.annotation.tailrec
 
@@ -30,6 +31,16 @@ class ContinuationsPhase extends PluginPhase:
   override val runsAfter = Set(Staging.name)
   override val runsBefore = Set(PickleQuotes.name)
 
+  override def transformDefDef(tree: tpd.DefDef)(using ctx: Context): Tree =
+    tree match
+      case t if returnsContextFunctionWithSuspendType(t) =>
+        report.logWith("transformDefDef - new tree")(tree.show)
+        report.logWith("transformDefDef - new tree")(tree)
+
+      case _ =>
+        report.logWith("transformDefDef - original tree")(tree.show)
+        report.logWith("transformDefDef - original tree")(tree)
+
   override def transformBlock(tree: Block)(using ctx: Context): Tree =
 //    //val state = suspensionState(tree)
 //    report.error("DEEP FOLD")
@@ -42,12 +53,9 @@ class ContinuationsPhase extends PluginPhase:
 //      report.error(tree.show)
 //      acc
 //    })
-    // tree
-
-    report.logWith("transformBlock")(tree)
-    report.logWith("transformBlock")(tree.show)
     tree
 
+  /*
   @tailrec final def transformStatements(
       block: Block,
       statements: List[Tree],
@@ -60,17 +68,30 @@ class ContinuationsPhase extends PluginPhase:
           transformStatements(newBlock, remaining, Nil)
         // TODO nest previous under suspension point. Look at trees dif with example function
         else transformStatements(block, remaining, previous :+ current) // this may be wrong
+   */
 
   def isSuspendType(tpe: Type)(using ctx: Context): Boolean =
     tpe.classSymbol.showFullName == "continuations.Suspend"
 
-  def returnsContextFunctionWithSuspendType(tree: Tree)(using ctx: Context): Boolean =
-    ctx.definitions.isContextFunctionType(tree.tpe) && tree.tpe.argTypes.exists(isSuspendType)
+  def returnsContextFunctionWithSuspendType(tree: DefDef)(using ctx: Context): Boolean =
+    if (hasSuspendParam(tree)) tree.rhs.existsSubTree(hasInnerSuspensionPoint)
+    else false
 
-  def hasInnerSuspensionPoint(statement: Tree)(using ctx: Context): Boolean =
-    statement.find(isCallToSuspend).isDefined
+  def hasSuspendParam(tree: tpd.DefDef)(using ctx: Context): Boolean =
+    tree.paramss.reverse match {
+      case ValDefs(vparams @ (vparam :: _)) :: _
+          if vparam.mods.isOneOf(Flags.GivenOrImplicit) =>
+        vparams.exists(vp => isSuspendType(vp.tpe))
+      case _ => false
+    }
+
+  def hasInnerSuspensionPoint(subTree: Tree)(using ctx: Context): Boolean =
+    subTree match
+      case Inlined(Apply(fun, _), _, _) => isCallToSuspend(fun)
+      case Apply(fun, _) => isCallToSuspend(fun)
+      case _ => false
 
   def isCallToSuspend(tree: Tree)(using ctx: Context): Boolean =
-    tree match
-      case Apply(_, _) => returnsContextFunctionWithSuspendType(tree)
-      case _ => false
+    tree.symbol.name.show == "suspendContinuationOrReturn"
+
+end ContinuationsPhase
--- continuationsPlugin/src/test/scala/continuations/ContinuationsPluginSuite.scala
+++ continuationsPlugin/src/test/scala/continuations/ContinuationsPluginSuite.scala
@@ -1,8 +1,15 @@
 package continuations
 
 import dotty.tools.dotc.ast.tpd
-import dotty.tools.dotc.core.Contexts.Context
-import dotty.tools.dotc.core.Symbols.ClassSymbol
+import dotty.tools.dotc.core.Contexts.{Context, ctx}
+import dotty.tools.dotc.core.Symbols
+import dotty.tools.dotc.core.Symbols.{ClassSymbol, newSymbol}
+import dotty.tools.dotc.core.{Flags, Names}
+import dotty.tools.dotc.core.Flags.EmptyFlags
+import dotty.tools.dotc.core.Constants.Constant
+import dotty.tools.dotc.ast.tpd.*
+import dotty.tools.dotc.core.Names.termName
+import dotty.tools.dotc.core.Types.*
 import munit.FunSuite
 
 /**
@@ -41,7 +48,207 @@ class ContinuationsPluginSuite extends FunSuite, CompilerFixtures {
 
   }
 
-  compilerContext.test("it should run the compiler") { implicit givenContext =>
+  compilerContextWithContinuationsPlugin.test(
+    "It should convert a suspended def with a single constant and a non suspended body to CPS"
+  ) { implicit givenContext =>
+    val source =
+      """
+        |package continuations
+        |
+        |def foo(x: Int)(using Suspend): Int = x + 1
+        |""".stripMargin
+
+    val expected =
+      """
+        |package continuations {
+        |  final lazy module val compileFromString$package: 
+        |    continuations.compileFromString$package
+        |   = new continuations.compileFromString$package()
+        |  @SourceFile("compileFromString.scala") final module class 
+        |    compileFromString$package
+        |  () extends Object() { this: continuations.compileFromString$package.type =>
+        |    private def writeReplace(): AnyRef = 
+        |      new scala.runtime.ModuleSerializationProxy(classOf[continuations.compileFromString$package.type])
+        |    def foo(x: Int)(using x$2: continuations.Suspend): Int = x.+(1)
+        |  }
+        |}
+        |""".stripMargin
+      
+    checkContinuations(source) {
+      case (tree, _) =>
+        assertNoDiff(compileSourceIdentifier.replaceAllIn(tree.show, ""), expected)
+    }
+  }
+
+  compilerContextWithContinuationsPlugin.test(
+    "returnsContextFunctionWithSuspendType should return true for a context function with a call to suspend"
+  ) { implicit givenContext =>
+
+    val source =
+      """
+        |package continuations
+        |
+        |inline def suspendContinuationOrReturn[A](f: Continuation[A] => Unit): A = ???
+        |
+        |def foo(x: Int): Suspend ?=> Int =
+        |  suspendContinuationOrReturn { (c: Continuation[Int]) =>
+        |    c.resume(Right(x))
+        |    Continuation.State.Suspended
+        |  }
+        |
+        |""".stripMargin
+
+    checkContinuations(source) {
+      case (tree, context) =>
+        given Context = context
+
+        val funName = Names.termName("$anonfun")
+
+        val defDefs = tree.filterSubTrees {
+          case DefDef(name, _, _, _) if name == funName => true
+          case _ => false
+        }
+        
+        val foo = defDefs.head.asInstanceOf[tpd.DefDef]
+        
+        val sut = new ContinuationsPhase
+        assertEquals(sut.returnsContextFunctionWithSuspendType(foo), true)
+    }
+  }
+
+  compilerContextWithContinuationsPlugin.test(
+    "returnsContextFunctionWithSuspendType should return false for a context function with no call to suspend"
+  ) { implicit givenContext =>
+
+    val source =
+      """
+        |package continuations
+        |
+        |inline def suspendContinuationOrReturn[A](f: Continuation[A] => Unit): A = ???
+        |
+        |def foo(x: Int): Suspend ?=> Int = x + 1
+        |
+        |""".stripMargin
+
+    checkContinuations(source) {
+      case (tree, context) =>
+        given Context = context
+
+        val funName = Names.termName("$anonfun")
+
+        val defDefs = tree.filterSubTrees {
+          case DefDef(name, _, _, _) if name == funName => true
+          case _ => false
+        }
+
+        val foo = defDefs.head.asInstanceOf[tpd.DefDef]
+
+        val sut = new ContinuationsPhase
+        assertEquals(sut.returnsContextFunctionWithSuspendType(foo), false)
+    }
+  }
+
+  compilerContextWithContinuationsPlugin.test(
+    "returnsContextFunctionWithSuspendType should return true for a function using Suspend with a call to suspend"
+  ) { implicit givenContext =>
+
+    val source =
+      """
+        |package continuations
+        |
+        |inline def suspendContinuationOrReturn[A](f: Continuation[A] => Unit): A = ???
+        |
+        |def foo(x: Int)(using s: Suspend): Int =
+        |  suspendContinuationOrReturn { (c: Continuation[Int]) =>
+        |    c.resume(Right(x))
+        |    Continuation.State.Suspended
+        |  }
+        |
+        |""".stripMargin
+
+    checkContinuations(source) {
+      case (tree, context) =>
+        given Context = context
+
+        val funName = Names.termName("foo")
+
+        val defDefs = tree.filterSubTrees {
+          case DefDef(name, _, _, _) if name == funName => true
+          case _ => false
+        }
+
+        val foo = defDefs.head.asInstanceOf[tpd.DefDef]
+
+        val sut = new ContinuationsPhase
+        assertEquals(sut.returnsContextFunctionWithSuspendType(foo), true)
+    }
+  }
+
+  compilerContextWithContinuationsPlugin.test(
+    "returnsContextFunctionWithSuspendType should return false for a function using Suspend with no call to suspend"
+  ) { implicit givenContext =>
+
+    val source =
+      """
+        |package continuations
+        |
+        |inline def suspendContinuationOrReturn[A](f: Continuation[A] => Unit): A = ???
+        |
+        |def foo(x: Int)(using s: Suspend): Int = x + 1
+        |
+        |""".stripMargin
+
+    checkContinuations(source) {
+      case (tree, context) =>
+        given Context = context
+
+        val funName = Names.termName("foo")
+
+        val defDefs = tree.filterSubTrees {
+          case DefDef(name, _, _, _) if name == funName => true
+          case _ => false
+        }
+
+        val foo = defDefs.head.asInstanceOf[tpd.DefDef]
+
+        val sut = new ContinuationsPhase
+        assertEquals(sut.returnsContextFunctionWithSuspendType(foo), false)
+    }
+  }
+
+  compilerContextWithContinuationsPlugin.test(
+    "returnsContextFunctionWithSuspendType should return false for a function with Suspend as a parameter"
+  ) { implicit givenContext =>
+
+    val source =
+      """
+        |package continuations
+        |
+        |inline def suspendContinuationOrReturn[A](f: Continuation[A] => Unit): A = ???
+        |
+        |def foo(x: Int)(s: Suspend): Int = x + 1
+        |
+        |""".stripMargin
+
+    checkContinuations(source) {
+      case (tree, context) =>
+        given Context = context
+
+        val funName = Names.termName("foo")
+
+        val defDefs = tree.filterSubTrees {
+          case DefDef(name, _, _, _) if name == funName => true
+          case _ => false
+        }
+
+        val foo = defDefs.head.asInstanceOf[tpd.DefDef]
+
+        val sut = new ContinuationsPhase
+        assertEquals(sut.returnsContextFunctionWithSuspendType(foo), false)
+    }
+  }
+
+  compilerContext.test("It should run the compiler") { implicit givenContext =>
     val source = """
                    |class A
                    |class B extends A
@@ -68,7 +275,7 @@ class ContinuationsPluginSuite extends FunSuite, CompilerFixtures {
         assert(lb <:< la)
         assert(!(la <:< lb))
 
-      case _ => fail("no list or context compiled from source ${source}, ${types}")
+      case _ => fail(s"no list or context compiled from source ${source}, ${types}")
     }
   }
 
