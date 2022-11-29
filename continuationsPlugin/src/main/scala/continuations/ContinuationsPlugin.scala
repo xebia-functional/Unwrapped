16a09ff1940fd60bf3f153839552e43d58c0be41
Jack C. Viers <jackcviers@gmail.com>
2022-11-28 21:05:25 -0600

    WIP - Initializing transform counter

--- continuationsPlugin/src/main/scala/continuations/ContinuationsPlugin.scala
+++ continuationsPlugin/src/main/scala/continuations/ContinuationsPlugin.scala
@@ -6,6 +6,11 @@ import dotty.tools.dotc.plugins.PluginPhase
 import dotty.tools.dotc.plugins.StandardPlugin
 import dotty.tools.dotc.transform.PickleQuotes
 import dotty.tools.dotc.transform.Staging
+import dotty.tools.dotc.ast.tpd
+import dotty.tools.dotc.util.Store.Location
+import dotty.tools.dotc.util.Store
+import dotty.tools.dotc.util.Property.Key
+import dotty.tools.dotc.util.Property.StickyKey
 
 class ContinuationsPlugin extends StandardPlugin:
 
@@ -26,7 +31,59 @@ class ContinuationsPhase extends PluginPhase:
   override val runsAfter = Set(Staging.name)
   override val runsBefore = Set(PickleQuotes.name)
 
+  //format: off
+  /**
+    * Adds the synthetic counter to the context when the phase is entered on a compilation unit. The counter starts at 1. It can be fetched with:
+    *
+    ```scala
+      summon[Context].property(ContinuationsPhase.continuationsPhaseCounterPropertyKey).get
+    ```
+    * 
+    * The value in the location should only be updated in a
+    * `prepareFor<N>`. This means that we need to know how many
+    * synthetics we will need to generate in the N transform. This
+    * shuould be possible via scanning source, then in the transform,
+    * working backword from currently set property value. To update in
+    * a `prepareFor<N>`:
+    * 
+    ```scala
+    val newContext = ctx.fresh
+    val counterLocation = newContext.property(ContinuationsPhase.continuationsPhaseCounterPropertyKey).get
+    val oldCount = newContext.store(counterLocation)
+    // scan for necessary synthetics by walking the tree and adding to the old count,
+    // returning the new context
+    newContext.updateStore(counterLocation, countNecessarySynthetics(oldCount, tree))
+    ```
+    *
+    * @param tree
+    * @param ctx
+    * @return
+    */
+  //format: on
+  override def prepareForUnit(tree: tpd.Tree)(using ctx: Context): Context = {
+    val newContext = ctx.fresh
+    val key = ContinuationsPhase.continuationsPhaseCounterPropertyKey
+    val contextPropertyCounter = newContext.property(key)
+    if (contextPropertyCounter.isDefined) {
+      newContext
+    } else {
+      val counterLocation = newContext.addLocation(1)
+      newContext.setProperty(key, counterLocation)
+    }
+  }
+
+  override def prepareForDefDef(tree: tpd.DefDef)(using ctx:Context): Context =
+    val newContext = ctx.fresh
+    val counterLocation = newContext.property(ContinuationsPhase.continuationsPhaseCounterPropertyKey).get
+    val oldCount = newContext.store(counterLocation)
+    newContext.updateStore(counterLocation, DefDefTransforms.countContinuationSynthetics(tree, oldCount)(using newContext))
+
   override def transformDefDef(tree: DefDef)(using ctx: Context): Tree =
     DefDefTransforms.transformSuspendContinuation(tree)
 
 end ContinuationsPhase
+
+object ContinuationsPhase {
+  val continuationsPhaseCounterPropertyKey =
+    new StickyKey[Location[Int]]{}
+}
--- continuationsPlugin/src/main/scala/continuations/DefDefTransforms.scala
+++ continuationsPlugin/src/main/scala/continuations/DefDefTransforms.scala
@@ -1,45 +1,36 @@
 package continuations
 
-import continuations.DefDefTransforms.*
 import dotty.tools.dotc.ast.Trees
-import dotty.tools.dotc.ast.Trees.*
-import dotty.tools.dotc.ast.tpd
-import dotty.tools.dotc.ast.tpd.TypedTreeCopier
 import dotty.tools.dotc.ast.tpd.*
 import dotty.tools.dotc.core.Constants.Constant
 import dotty.tools.dotc.core.Contexts.Context
 import dotty.tools.dotc.core.Contexts.ctx
-import dotty.tools.dotc.core.Flags
-import dotty.tools.dotc.core.Names
+import dotty.tools.dotc.core.Flags.GivenOrImplicit
+import dotty.tools.dotc.core.Flags.Local
+import dotty.tools.dotc.core.Flags.LocalParam
 import dotty.tools.dotc.core.Names.termName
-import dotty.tools.dotc.core.StdNames.nme
+import dotty.tools.dotc.core.StdNames.nme.CONSTRUCTOR
 import dotty.tools.dotc.core.Symbols.*
-import dotty.tools.dotc.core.Types
 import dotty.tools.dotc.core.Types.OrType
 import dotty.tools.dotc.core.Types.Type
 import dotty.tools.dotc.report
-
-import scala.annotation.tailrec
-import scala.collection.immutable.List
-import scala.collection.mutable.ListBuffer
 
 object DefDefTransforms:
 
   private def generateCompletion(owner: Symbol, returnType: Type)(using Context): Symbol =
     newSymbol(
       owner,
-      Names.termName("completion"),
-      Flags.LocalParam,
+      termName("completion"),
+      LocalParam,
       requiredPackage("continuations")
         .requiredType("Continuation")
         .typeRef
         .appliedTo(returnType)
     )
 
-  private def params(tree: tpd.DefDef, completionSym: Symbol)(
-      using Context): List[tpd.ParamClause] =
+  private def params(tree: DefDef, completionSym: Symbol)(using Context): List[ParamClause] =
     val suspendClazz = requiredClass(suspendFullName)
-    val completion: tpd.ValDef = tpd.ValDef(completionSym.asTerm, theEmptyTree)
+    val completion: ValDef = ValDef(completionSym.asTerm, Trees.theEmptyTree)
 
     if (tree.paramss.isEmpty) {
       List(List(completion))
@@ -48,28 +39,44 @@ object DefDefTransforms:
         .paramss
         .zipWithIndex
         .map { (pc, i) =>
-          val newPc: tpd.ParamClause = pc
+          val newPc: ParamClause = pc
             .filterNot {
               case p: Trees.ValDef[Type] =>
                 p.typeOpt
-                  .hasClassSymbol(suspendClazz) && p.symbol.flags.isOneOf(Flags.GivenOrImplicit)
+                  .hasClassSymbol(suspendClazz) && p.symbol.flags.isOneOf(GivenOrImplicit)
               case t: Trees.TypeDef[Type] =>
                 t.typeOpt
-                  .hasClassSymbol(suspendClazz) && t.symbol.flags.isOneOf(Flags.GivenOrImplicit)
+                  .hasClassSymbol(suspendClazz) && t.symbol.flags.isOneOf(GivenOrImplicit)
             }
-            .asInstanceOf[tpd.ParamClause]
+            .asInstanceOf[ParamClause]
           if (i == 0) {
-            newPc.appended(completion).asInstanceOf[tpd.ParamClause]
+            newPc.appended(completion).asInstanceOf[ParamClause]
           } else newPc
         }
         .filterNot(_.isEmpty)
+
+  /**
+   * For each suspended defdef with dependent calculations, there will need to be 1 state
+   * machine class, a result variable, an initial continuation, a suspension continuation, a
+   * result continuation, and a result label for each suspension continuation. So N + 6 +
+   * oldCount.
+   *
+   * @param defdef
+   *   The [[dotty.tools.dotc.ast.tpd.DefDef]] to transform
+   * @param oldCount
+   *   The value of the previous counter.
+   * @return
+   *   The sum of the count and the old count.
+   */
+  def countContinuationSynthetics(defdef: DefDef, oldCount: Int)(using Context): Int =
+    ???
 
   /*
    * It works with only one top level `suspendContinuation` that just calls `resume`
    * and it replaces the parent method body keeping any rows before the `suspendContinuation` (but not ones after).
    */
-  private def transformSuspendOneContinuationResume(tree: tpd.DefDef, resumeArg: tpd.Tree)(
-      using Context): tpd.DefDef =
+  private def transformSuspendNoParametersOneContinuationResume(tree: DefDef, resumeArg: Tree)(
+      using Context): DefDef =
     val continuationTraitSym: ClassSymbol =
       requiredClass("continuations.Continuation")
     val continuationObjectSym: Symbol =
@@ -110,9 +117,9 @@ object DefDefTransforms:
 
     val completion = generateCompletion(parent, returnType)
 
-    val continuation1: tpd.ValDef =
-      tpd.ValDef(
-        sym = newSymbol(parent, termName("continuation1"), Flags.Local, continuationTyped),
+    val continuation1: ValDef =
+      ValDef(
+        sym = newSymbol(parent, termName("continuation1"), Local, continuationTyped),
         rhs = ref(completion))
 
     val undecidedState =
@@ -125,32 +132,23 @@ object DefDefTransforms:
         .appliedToNone
 
     val safeContinuationConstructor =
-      tpd
-        .New(ref(safeContinuationClassSym))
-        .select(nme.CONSTRUCTOR)
+      New(ref(safeContinuationClassSym))
+        .select(CONSTRUCTOR)
         .appliedToType(returnType)
         .appliedTo(interceptedCall, undecidedState)
 
-    val safeContinuation: tpd.ValDef =
-      tpd.ValDef(
-        newSymbol(
-          parent,
-          termName("safeContinuation"),
-          Flags.Local,
-          safeContinuationConstructor.tpe),
+    val safeContinuation: ValDef =
+      ValDef(
+        newSymbol(parent, termName("safeContinuation"), Local, safeContinuationConstructor.tpe),
         safeContinuationConstructor)
 
     val safeContinuationRef =
       ref(safeContinuation.symbol)
 
-    val suspendContinuation: tpd.ValDef =
-      tpd.ValDef(
-        newSymbol(
-          parent,
-          termName("suspendContinuation"),
-          Flags.Local,
-          ctx.definitions.IntType),
-        tpd.Literal(Constant(0))
+    val suspendContinuation: ValDef =
+      ValDef(
+        newSymbol(parent, termName("suspendContinuation"), Local, ctx.definitions.IntType),
+        Literal(Constant(0))
       )
 
     val suspendContinuationResume =
@@ -159,7 +157,7 @@ object DefDefTransforms:
     val suspendContinuationGetThrow =
       safeContinuationRef.select(termName("getOrThrow")).appliedToNone
 
-    val body = tpd.Block(
+    val body = Block(
       rowsBeforeSuspend ++
         (continuation1 :: safeContinuation :: suspendContinuation :: suspendContinuationResume :: Nil),
       suspendContinuationGetThrow
@@ -168,8 +166,8 @@ object DefDefTransforms:
     val suspendedState =
       ref(continuationObjectSym).select(termName("State")).select(termName("Suspended"))
 
-    val finalMethodReturnType: tpd.TypeTree =
-      tpd.TypeTree(
+    val finalMethodReturnType: TypeTree =
+      TypeTree(
         OrType(
           OrType(ctx.definitions.AnyType, ctx.definitions.NullType, soft = false),
           suspendedState.symbol.namedType,
@@ -182,7 +180,109 @@ object DefDefTransforms:
       rhs = body
     )
 
-  private def transformContinuationWithSuspend(tree: tpd.DefDef)(using Context): tpd.DefDef =
+  private def transformSuspendOneContinuationResume(tree: DefDef, resumeArg: Tree)(
+      using Context): DefDef =
+    val continuationTraitSym: ClassSymbol =
+      requiredClass("continuations.Continuation")
+    val continuationObjectSym: Symbol =
+      continuationTraitSym.companionModule
+    val safeContinuationClassSym: ClassSymbol =
+      requiredClass("continuations.SafeContinuation")
+    val interceptedMethodSym: TermSymbol =
+      requiredPackage("continuations.intrinsics").requiredMethod("intercepted")
+
+    val parent: Symbol = tree.symbol
+    val returnType: Type =
+      tree
+        .rhs
+        .find {
+          case Trees.Inlined(fun, _, _) =>
+            fun.symbol.showFullName == suspendContinuationFullName
+          case _ => false
+        }
+        .map(_.tpe)
+        .get
+
+    val rowsBeforeSuspend =
+      tree
+        .rhs
+        .toList
+        .flatMap {
+          case Trees.Block(trees, tree) => trees :+ tree
+          case tree => List(tree)
+        }
+        .takeWhile {
+          case Trees.Inlined(call, _, _) =>
+            call.symbol.showFullName != suspendContinuationFullName
+          case _ => true
+        }
+
+    val continuationTyped: Type =
+      continuationTraitSym.typeRef.appliedTo(returnType)
+
+    val completion =
+      newSymbol(parent, termName("completion"), LocalParam, continuationTyped)
+
+    val continuation1: ValDef =
+      ValDef(
+        newSymbol(parent, termName("continuation1"), Local, continuationTyped),
+        ref(completion))
+
+    val undecidedState =
+      ref(continuationObjectSym).select(termName("State")).select(termName("Undecided"))
+
+    val interceptedCall =
+      ref(interceptedMethodSym)
+        .appliedToType(returnType)
+        .appliedTo(ref(continuation1.symbol))
+        .appliedToNone
+
+    val safeContinuationConstructor =
+      New(ref(safeContinuationClassSym))
+        .select(CONSTRUCTOR)
+        .appliedToType(returnType)
+        .appliedTo(interceptedCall, undecidedState)
+
+    val safeContinuation: ValDef =
+      ValDef(
+        newSymbol(parent, termName("safeContinuation"), Local, safeContinuationConstructor.tpe),
+        safeContinuationConstructor)
+
+    val safeContinuationRef =
+      ref(safeContinuation.symbol)
+
+    val suspendContinuation: ValDef =
+      ValDef(
+        newSymbol(parent, termName("suspendContinuation"), Local, ctx.definitions.IntType),
+        Literal(Constant(0))
+      )
+
+    val suspendContinuationResume =
+      safeContinuationRef.select(termName("resume")).appliedTo(resumeArg)
+
+    val suspendContinuationGetThrow =
+      safeContinuationRef.select(termName("getOrThrow")).appliedToNone
+
+    val body = Block(
+      rowsBeforeSuspend ++
+        (continuation1 :: safeContinuation :: suspendContinuation :: suspendContinuationResume :: Nil),
+      suspendContinuationGetThrow
+    )
+
+    val suspendedState =
+      ref(continuationObjectSym).select(termName("State")).select(termName("Suspended"))
+
+    val finalMethodReturnType: TypeTree =
+      TypeTree(
+        OrType(
+          OrType(ctx.definitions.AnyType, ctx.definitions.NullType, soft = false),
+          suspendedState.symbol.namedType,
+          soft = false)
+      )
+
+    DefDef(parent.asTerm, List(List(completion)), finalMethodReturnType.tpe, body)
+
+  private def transformContinuationWithSuspend(tree: DefDef)(using Context): DefDef =
     val newTree =
       tree match
         case CallsContinuationResumeWith(resumeArg) =>
@@ -193,16 +293,16 @@ object DefDefTransforms:
 
     report.logWith(s"new tree:")(newTree)
 
-  def transformSuspendContinuation(tree: tpd.DefDef)(using Context): tpd.DefDef =
+  def transformSuspendContinuation(tree: DefDef)(using Context): DefDef =
     tree match
       case ReturnsContextFunctionWithSuspendType(_) => transformContinuationWithSuspend(tree)
       case HasSuspendParameter(_) => transformContinuationWithSuspend(tree)
       case _ => report.logWith(s"oldTree:")(tree)
 
-  private def transformNonSuspending(tree: tpd.DefDef, cpy: TypedTreeCopier)(
-      using ctx: Context): tpd.DefDef =
-    val returnType = Types.OrType(tree.tpt.tpe, ctx.definitions.AnyType, false)
+  private def transformNonSuspending(tree: DefDef, cpy: TypedTreeCopier)(
+      using ctx: Context): DefDef =
+    val returnType = OrType(tree.tpt.tpe, ctx.definitions.AnyType, false)
     val completion = generateCompletion(tree.symbol, returnType)
     cpy.DefDef(tree)(
       paramss = params(tree, completion),
-      tpt = tpd.TypeTree(ctx.definitions.ObjectClass.typeRef))
+      tpt = TypeTree(ctx.definitions.ObjectClass.typeRef))
