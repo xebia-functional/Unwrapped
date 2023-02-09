7370e6f32fbe228ad1c32d86c690e1de50e23cb0
Jack C. Viers <jackcviers@gmail.com>
2023-02-09 13:26:33 -0600

    cu-865bm99yr: Extracts some examples to projects
    This adds the ability to show the trees when the project is compiled

---
+++ list-map/src/main/scala/examples/ListMap.scala
@@ -0,0 +1,12 @@
+package examples
+
+import continuations.Suspend
+
+@main def ListMap =
+  def twoArgumentsOneContinuationsCFBefore(x: Int, y: Int): Suspend ?=> Int =
+    val z = 1
+    summon[Suspend].suspendContinuation[Int] { continuation =>
+      continuation.resume(Right(x + y + z))
+    }
+  val mappedContinuations = List(1, 2, 3, 4).map(twoArgumentsOneContinuationsCFBefore(1, _))
+  println(mappedContinuations)
---
+++ zero-arguments-no-continuation-treeview/src/main/scala/ZeroArgumentsZeroContinuations.scala
@@ -0,0 +1,9 @@
+package examples
+
+import continuations.*
+
+@main def ZeroArgumentsZeroContinuations = 
+  def zeroArgumentsZeroContinuations()(using Suspend): Int = 1
+  println(zeroArgumentsZeroContinuations())
+
+
