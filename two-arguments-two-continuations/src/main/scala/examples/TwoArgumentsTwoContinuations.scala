05056c7fb04c95ed03fbbdfbf8564554bc1f6b7f
Jack C. Viers <jackcviers@gmail.com>
2023-04-06 17:36:54 -0500

    Fixes normal def uncurrying

--- two-arguments-two-continuations/src/main/scala/examples/TwoArgumentsTwoContinuations.scala
+++ two-arguments-two-continuations/src/main/scala/examples/TwoArgumentsTwoContinuations.scala
@@ -5,17 +5,8 @@ import continuations.Suspend
 
 // val foo:  Suspend ?=> [A] => List[A] => Int = [A] => (list: List[A]) => list.size
 
-def threeDependentContinuations(a: Int, b: Int, c: Int)(using s: Suspend): Int =
-  val d = 4
-  val continuationOne: Int = s.shift(_.resume(d + a)) // 5
-  val e = 5
-  val continuationTwo: Int =
-    s.shift(_.resume(continuationOne + e + b)) // 12
-  val f = 6
-  val result: Int = s.shift(_.resume(continuationTwo + f + c)) // 21
-  result
-
-
+def threeDependentContinuations[A](x: A)(using s: Suspend): A =
+  s.shift(_.resume(x)) // 5
 
 // val polymorphicValDefZeroContinuations
 //     : Suspend ?=> [A] => List[A] => [B] => List[B] => List[A] => List[A] => Int =
@@ -31,7 +22,7 @@ def threeDependentContinuations(a: Int, b: Int, c: Int)(using s: Suspend): Int =
 
 @main def main =
   // println(foo(List(1)))
-  println(threeDependentContinuations(1, 2, 3))
+  println(threeDependentContinuations(1))
   // println(
   //   polymorphicValDefZeroContinuations(List(1))(List("A", "B"))(List(1, 1, 1))(
   //     List(1, 1, 1, 1)))
